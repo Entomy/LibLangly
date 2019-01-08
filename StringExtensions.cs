d8d8d55d2372971edf80c63a139802ef715bb7a9
Patrick Kelly <p-kell@live.com>
2019-01-07 21:49:09 -0500

    Removed SubstringSpan, as it's not worth it

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -33,22 +33,6 @@ namespace Stringier {
 		/// <returns>A collection of the Match objects found by the search. If no matches are found, the method returns an empty collection object.</returns>
 		public static MatchCollection Matches(this String String, String Pattern, RegexOptions RegexOptions = RegexOptions.None) => Regex.Matches(String, Pattern, RegexOptions);
 
-		/// <summary>
-		/// Retrieves a substring from this instance. The substring starts at a specified character position and continues to the end of the string.
-		/// </summary>
-		/// <param name="String"></param>
-		/// <param name="StartIndex">The zero-based starting character position of a substring in this instance.</param>
-		/// <returns>A string that is equivalent to the substring that begins at startIndex in this instance, or Empty if startIndex is equal to the length of this instance.</returns>
-		public static ReadOnlySpan<Char> SubstringSpan(this String String, Int32 StartIndex) => String.Substring(StartIndex).AsSpan();
-
-		/// <summary>
-		/// Retrieves a substring from this instance. The substring starts at a specified character position and has a specified length.
-		/// </summary>
-		/// <param name="String"></param>
-		/// <param name="StartIndex">The zero-based starting character position of a substring in this instance.</param>
-		/// <param name="Length">The number of characters in the substring.</param>
-		/// <returns>A string that is equivalent to the substring of length length that begins at startIndex in this instance, or Empty if startIndex is equal to the length of this instance and length is zero.</returns>
-		public static ReadOnlySpan<Char> SubstringSpan(this String String, Int32 StartIndex, Int32 Length) => String.Substring(StartIndex, Length).AsSpan();
 
 		/// <summary>
 		/// Converts the specified string to title case (except for words that are entirely in uppercase, which are considered to be acronyms).
