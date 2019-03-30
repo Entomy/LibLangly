6d611aeb5942f003cfcc77533309199626e0f0b4
Patrick Kelly <p-kell@live.com>
2019-03-30 08:45:32 -0400

    Added .Contains() for IEnumerable<String>

--- Stringier/StringExtensions.cs
+++ Stringier/StringExtensions.cs
@@ -68,6 +68,19 @@ namespace System {
 		public static Boolean Contains(this String String, Char Value) => String.Contains(Value.ToString());
 
 		/// <summary>
+		/// Returns a value indicating whether a specified char occurs within any of the strings.
+		/// </summary>
+		/// <param name="Strings">The enumeration of strings to check.</param>
+		/// <param name="Value">The char to seek.</param>
+		/// <returns>true if the value parameter occurs within any of the strings, or if the value is the empty char (''); otherwise, false.</returns>
+		public static Boolean Contains(this IEnumerable<String> Strings, Char Value) {
+			foreach (String String in Strings) {
+				if (String.Contains(Value)) return true;
+			}
+			return false;
+		}
+
+		/// <summary>
 		/// Ensures the <paramref name="String"/> beings with the <paramref name="Required"/> string, adding it if necessary.
 		/// </summary>
 		/// <param name="String">The string to ensure.</param>
