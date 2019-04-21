28151286d534219eb69ebda6733c25bed39ac374
Patrick Kelly <p-kell@live.com>
2019-04-21 16:41:17 -0400

    Implemented Char[],Join() methods which convert to String with optional separator characters

---
+++ Stringier/CharExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System {
+	public static class CharExtensions {
+
+		public static String Join(this Char[] Chars) => new String(Chars);
+
+		public static String Join(this Char[] Chars, Char Separator) {
+			Char[] Result = new Char[Chars.Length * 2 - 1];
+			for (int i = 0; i < Chars.Length * 2 - 1; i++) {
+				if (i % 2 == 0) {
+					Result[i] = Chars[i / 2];
+				} else {
+					Result[i] = Separator;
+				}
+			}
+			return Result.Join();
+		}
+
+	}
+}
---
+++ Tests/CharTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+	[TestClass]
+	public class CharTests {
+		[TestMethod]
+		public void Join() {
+			Assert.AreEqual("hello", new Char[]{ 'h', 'e', 'l', 'l', 'o' }.Join());
+			Assert.AreEqual("h e l l o", new Char[] { 'h', 'e', 'l', 'l', 'o' }.Join(' '));
+		}
+	}
+}
