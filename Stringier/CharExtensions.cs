fa4fcf1040f3378264e5cb2f73abf5935b0e47c1
Patrick Kelly <p-kell@live.com>
2019-05-02 21:25:51 -0400

    Added documentation

--- Stringier/CharExtensions.cs
+++ Stringier/CharExtensions.cs
@@ -5,8 +5,19 @@ using System.Text;
 namespace System {
 	public static class CharExtensions {
 
+		/// <summary>
+		/// Joins the <paramref name="Chars"/> into a string.
+		/// </summary>
+		/// <param name="Chars">The <see cref="Char[]"/> to join.</param>
+		/// <returns>The joined <see cref="String"/>.</returns>
 		public static String Join(this Char[] Chars) => new String(Chars);
 
+		/// <summary>
+		/// Joins the <paramref name="Chars"/> with <paramref name="Separator"/> into a string.
+		/// </summary>
+		/// <param name="Chars">The <see cref="Char[]"/> to join.</param>
+		/// <param name="Separator">The <see cref="Char"/> to interleave.</param>
+		/// <returns>The joined <see cref="String"/>.</returns>
 		public static String Join(this Char[] Chars, Char Separator) {
 			Char[] Result = new Char[Chars.Length * 2 - 1];
 			for (int i = 0; i < Chars.Length * 2 - 1; i++) {
