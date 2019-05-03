f0372f2065c000b38867bbd00919bd00128bdd0c
Patrick Kelly <p-kell@live.com>
2019-05-02 22:04:01 -0400

    Reworked Result, is now only a String with special logic, and will be the capture not the remaining portion

--- Stringier.Patterns/Result.cs
+++ Stringier.Patterns/Result.cs
@@ -1,22 +1,17 @@
 ﻿namespace System.Text.Patterns {
 	/// <summary>
-	/// Represents the result value of parsing operations. There are two main components. The first is a Boolean representing success of the operation. The second is the remaining string after the operation. This type can be used as if it is either individual type.
+	/// Represents the result value of parsing operations. There are two main components. The first is a Boolean representing success of the operation. The second is the captured string during the operation. This type can be used as if it is either individual type.
 	/// </summary>
-	public readonly struct Result : IEquatable<Result>, IEquatable<String> {
-		private readonly String Remaining;
+	public readonly struct Result : IEquatable<Result>, IEquatable<String>, IEquatable<Boolean> {
+		private readonly String Capture;
 
-		private readonly Boolean Success;
-
-		public Result(Boolean Success, String Remaining) {
-			this.Success = Success;
-			this.Remaining = Remaining;
+		public Result(String Capture) {
+			this.Capture = Capture;
 		}
 
-		public static implicit operator Boolean(Result Result) => Result.Success;
+		public static implicit operator Boolean(Result Result) => !String.IsNullOrEmpty(Result);
 
-		public static implicit operator Result(String Remaining) => new Result(true, Remaining);
-
-		public static implicit operator String(Result Result) => Result.Remaining;
+		public static implicit operator String(Result Result) => Result.Capture;
 
 		public static Boolean operator !=(Result left, Result right) => !left.Equals(right);
 
@@ -30,12 +25,14 @@
 
 		public static Boolean operator ==(String left, Result right) => right.Equals(left);
 
-		public Boolean Equals(Result other) => Success.Equals(other.Success) && String.Equals(Remaining, other.Remaining, StringComparison.Ordinal);
+		public Boolean Equals(Result other) => String.Equals(Capture, other.Capture, StringComparison.Ordinal);
 
-		public Boolean Equals(String other) => String.Equals(Remaining, other, StringComparison.InvariantCulture);
+		public Boolean Equals(String other) => String.Equals(Capture, other, StringComparison.InvariantCulture);
 
 		public override Boolean Equals(Object obj) {
 			switch (obj) {
+			case Boolean Other:
+				return Equals(Other);
 			case String Other:
 				return Equals(Other);
 			case Result Other:
@@ -45,6 +42,8 @@
 			}
 		}
 
-		public override Int32 GetHashCode() => Remaining.GetHashCode() ^ Success.GetHashCode();
+		public Boolean Equals(Boolean other) => other.Equals(!String.IsNullOrEmpty(Capture));
+
+		public override Int32 GetHashCode() => Capture.GetHashCode();
 	}
 }
