8c807662461bc737da7faea667047440aae90e9b
Patrick Kelly <p-kell@live.com>
2019-08-01 17:50:11 -0400

    Restored all benchmarks

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -4,10 +4,10 @@ using BenchmarkDotNet.Running;
 namespace Benchmarks {
 	class Program {
 		static void Main() {
-			/*BenchmarkRunner.Run<AlternatorComparison>();
+			BenchmarkRunner.Run<AlternatorComparison>();
 			BenchmarkRunner.Run<CombinatorComparison>();
 			BenchmarkRunner.Run<LiteralComparison>();
-			BenchmarkRunner.Run<OptorComparison>();*/
+			BenchmarkRunner.Run<OptorComparison>();
 			BenchmarkRunner.Run<RepeaterComparison>();
 			BenchmarkRunner.Run<SpannerComparison>();
 		}
