1406513b20f1b96496bbe6bb2f2f7f0614c3e55f
Patrick Kelly <p-kell@live.com>
2019-08-10 08:31:45 -0400

    Update exception message

--- Tests/AssertExtensions.cs
+++ Tests/AssertExtensions.cs
@@ -6,17 +6,17 @@ namespace Tests {
 	internal static class AssertExtensions {
 
 		public static void Fails(this Assert Assert, Result Actual) {
-			if (Actual) { throw new AssertFailedException("ResultAssert.Fails failed"); }
+			if (Actual) { throw new AssertFailedException("Assert.That.Fails failed"); }
 		}
 
 		public static void Captures(this Assert Assert, String Expected, Result Actual) {
 			if (Actual != Expected) {
-				throw new AssertFailedException($"ResultAssert.Captures failed. Expected: <{Expected}>. Actual: <{(String)Actual}>.");
+				throw new AssertFailedException($"Assert.That.Captures failed. Expected: <{Expected}>. Actual: <{(String)Actual}>.");
 			}
 		}
 
 		public static void Succeeds(this Assert Assert, Result Actual) {
-			if (!Actual) { throw new AssertFailedException("ResultAssert.Succeeds failed."); }
+			if (!Actual) { throw new AssertFailedException("Assert.That.Succeeds failed."); }
 		}
 	}
 }
