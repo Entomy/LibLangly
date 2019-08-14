1e25d9e8bb07705e37df3245e6fb97a71dd5da36
Patrick Kelly <p-kell@live.com>
2019-08-14 08:32:26 -0400

    Implemented Int16.Parse mapping

--- Stringier/StringExtensions.cs
+++ Stringier/StringExtensions.cs
@@ -376,6 +376,38 @@ namespace System {
 		}
 
 		/// <summary>
+		/// Converts the string representation of a number in a specified style and culture-specific format to its 16-bit signed integer equivalent.
+		/// </summary>
+		/// <param name="String">A string containing a number to convert.</param>
+		/// <param name="Style">A bitwise combination of enumeration values that indicates the style elements that can be present in <paramref name="String"/>. A typical value to specify is <see cref="NumberStyles.Integer"/>.</param>
+		/// <param name="Provider">An object that supplies culture-specific information about the format of <paramref name="String"/>.</param>
+		/// <returns>A 16-bit signed integer equivalent to the number specified in <paramref name="String"/>.</returns>
+		public static Int16 ParseInt16(this String String, NumberStyles Style, IFormatProvider Provider) => Int16.Parse(String, Style, Provider);
+
+		/// <summary>
+		/// Converts the string representation of a number in a specified style to its 16-bit signed integer equivalent.
+		/// </summary>
+		/// <param name="String">A string containing a number to convert.</param>
+		/// <param name="Style">A bitwise combination of the enumeration values that indicates the style elements that can be present in <paramref name="String"/>. A typical value to specify is <see cref="NumberStyles.Integer"/>.</param>
+		/// <returns>A 16-bit signed integer equivalent to the number specified in <paramref name="String"/>.</returns>
+		public static Int16 ParseInt16(this String String, NumberStyles Style) => Int16.Parse(String, Style);
+
+		/// <summary>
+		/// Converts the string representation of a number to its 16-bit signed integer equivalent.
+		/// </summary>
+		/// <param name="String">A string containing a number to convert.</param>
+		/// <returns>A 16-bit signed integer equivalent to the number contained in <paramref name="String"/>.</returns>
+		public static Int16 ParseInt16(this String String) => Int16.Parse(String);
+
+		/// <summary>
+		/// Converts the string representation of a number in a specified culture-specific format to its 16-bit signed integer equivalent.
+		/// </summary>
+		/// <param name="String">A string containing a number to convert.</param>
+		/// <param name="Provider">An object that supplies culture-specific formatting information about <paramref name="String"/>.</param>
+		/// <returns>A 16-bit signed integer equivalent to the number specified in <paramref name="String"/>.</returns>
+		public static Int16 ParseInt16(this String String, IFormatProvider Provider) => Int16.Parse(String, Provider);
+
+		/// <summary>
 		/// Converts the string representation of a number in a specified style and culture-specific format to its 32-bit signed integer equivalent.
 		/// </summary>
 		/// <param name="String">A string containing a number to convert.</param>
@@ -405,7 +437,7 @@ namespace System {
 		/// <param name="String">A string containing a number to convert.</param>
 		/// <param name="Provider">An object that supplies culture-specific formatting information about <paramref name="String"/>.</param>
 		/// <returns>A 32-bit signed integer equivalent to the number specified in <paramref name="String"/>.</returns>
-		public static Int32 ParseInt32(this String String, IFormatProvider Provider) => Int32.Parse(String);
+		public static Int32 ParseInt32(this String String, IFormatProvider Provider) => Int32.Parse(String, Provider);
 
 		/// <summary>
 		/// In the input string, replaces all strings that match a specified regular expression with a specified replacement string. Specified options modify the matching operation.
