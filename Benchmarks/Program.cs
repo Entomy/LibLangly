9bb36e2d4f14edcc116dfaccb20a96dcc7fc402d
Patrick Kelly <p-kell@live.com>
2019-08-23 12:15:18 -0400

    Renabled all benchmarks

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -4,18 +4,18 @@ using BenchmarkDotNet.Running;
 namespace Benchmarks {
 	static class Program {
 		static void Main() {
-			//BenchmarkRunner.Run<Int32ParseCharComparison>();
-			//BenchmarkRunner.Run<Int32ParseStringComparison>();
-			//BenchmarkRunner.Run<AlternatorComparison>();
-			//BenchmarkRunner.Run<ConcatenatorComparison>();
+			BenchmarkRunner.Run<Int32ParseCharComparison>();
+			BenchmarkRunner.Run<Int32ParseStringComparison>();
+			BenchmarkRunner.Run<AlternatorComparison>();
+			BenchmarkRunner.Run<ConcatenatorComparison>();
 			BenchmarkRunner.Run<CompoundPatternComparison>();
-			//BenchmarkRunner.Run<LiteralComparison>();
-			//BenchmarkRunner.Run<NegatorComparison>();
-			//BenchmarkRunner.Run<OptorComparison>();
+			BenchmarkRunner.Run<LiteralComparison>();
+			BenchmarkRunner.Run<NegatorComparison>();
+			BenchmarkRunner.Run<OptorComparison>();
 			BenchmarkRunner.Run<RangeComparison>();
-			//BenchmarkRunner.Run<RepeaterComparison>();
-			//BenchmarkRunner.Run<SpannerComparison>();
-			//BenchmarkRunner.Run<StringComparison>();
+			BenchmarkRunner.Run<RepeaterComparison>();
+			BenchmarkRunner.Run<SpannerComparison>();
+			BenchmarkRunner.Run<StringComparison>();
 		}
 	}
 }
