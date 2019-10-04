ecbacf425be4bfb5f9682635f9755351ec39ca59
Patrick Kelly <p-kell@live.com>
2019-10-04 10:15:55 -0400

    Substantially increased gibberish reduction factor in tests, so they run faster.

--- Tests.FSharp/GibberishTests.fs
+++ Tests.FSharp/GibberishTests.fs
@@ -11,7 +11,7 @@ open FParsec
 type GibberishTests () =
     [<TestMethod>]
     member this.PatternTest() =
-        let mutable source = new Source(Gibberish.Generate(32))
+        let mutable source = new Source(Gibberish.Generate(128))
         let word:Pattern = +(check "letter" (fun (char) -> 'a' <= char && char <= 'z'))
         let space:Pattern = +' '
         let doc:Pattern = +(word || space) >> Source.End
@@ -20,14 +20,14 @@ type GibberishTests () =
     [<TestMethod>]
     member this.RegexTest() =
         let regex = new Regex(@"(?:[a-z]+| +)+$", RegexOptions.Singleline);
-        Assert.IsTrue(regex.IsMatch(Gibberish.Generate(32)))
+        Assert.IsTrue(regex.IsMatch(Gibberish.Generate(128)))
 
     [<TestMethod>]
     member this.FParsecTest() =
         let word = many1Chars (anyOf "abcdefghijklmnopqrstuvwxyz")
         let space = many1Chars (pchar ' ')
         let doc = many(word <|> space) .>>. eof
-        let result = run doc (Gibberish.Generate(32))
+        let result = run doc (Gibberish.Generate(128))
         match result with
         | Success(_, _, _) -> ()
         | Failure(_, _, _) -> Assert.Fail()
