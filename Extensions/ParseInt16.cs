da13c093902be4beeba3b8594a7859c3239ac2dd
Patrick Kelly <p-kell@live.com>
2019-11-01 16:21:49 -0400

    Added ParseInt* variations for Char

---
+++ Extensions/ParseInt16.cs
@@ -0,0 +1,65 @@
+﻿using System.Globalization;
+
+namespace System {
+	public static partial class StringierExtensions {
+		/// <summary>
+		/// Converts the string representation of a number to its 16-bit signed integer equivalent.
+		/// </summary>
+		/// <param name="Char">A char containing a number to convert.</param>
+		/// <returns>A 16-bit signed integer equivalent to the number contained in <paramref name="Char"/>.</returns>
+		public static Int16 ParseInt16(this Char Char) => Char switch
+		{
+			'0' => (Int16)0,
+			'1' => (Int16)1,
+			'2' => (Int16)2,
+			'3' => (Int16)3,
+			'4' => (Int16)4,
+			'5' => (Int16)5,
+			'6' => (Int16)6,
+			'7' => (Int16)7,
+			'8' => (Int16)8,
+			'9' => (Int16)9,
+			_ => throw new FormatException("Char is not of the correct format."),
+		};
+
+		/// <summary>
+		/// Converts the string representation of a number to its 16-bit signed integer equivalent.
+		/// </summary>
+		/// <param name="Char">A char containing a number to convert.</param>
+		/// <param name="Style">A bitwise combination of the enumeration values that indicates the style elements that can be present in <paramref name="Char"/>. A typical value to specify is <see cref="NumberStyles.AllowHexSpecifier"/>.</param>
+		/// <returns>A 16-bit signed integer equivalent to the number contained in <paramref name="Char"/>.</returns>
+		public static Int16 ParseInt16(this Char Char, NumberStyles Style) {
+			if (Style.HasFlag(NumberStyles.AllowHexSpecifier)) {
+				return Char switch
+				{
+					'0' => (Int16)0x0,
+					'1' => (Int16)0x1,
+					'2' => (Int16)0x2,
+					'3' => (Int16)0x3,
+					'4' => (Int16)0x4,
+					'5' => (Int16)0x5,
+					'6' => (Int16)0x6,
+					'7' => (Int16)0x7,
+					'8' => (Int16)0x8,
+					'9' => (Int16)0x9,
+					'a' => (Int16)0xA,
+					'A' => (Int16)0xA,
+					'b' => (Int16)0xB,
+					'B' => (Int16)0xB,
+					'c' => (Int16)0xC,
+					'C' => (Int16)0xC,
+					'd' => (Int16)0xD,
+					'D' => (Int16)0xD,
+					'e' => (Int16)0xE,
+					'E' => (Int16)0xE,
+					'f' => (Int16)0xF,
+					'F' => (Int16)0xF,
+					_ => throw new FormatException("Char is not of the correct format."),
+				};
+			}
+			else {
+				return Char.ParseInt16();
+			}
+		}
+	}
+}
---
+++ Extensions/ParseInt32.cs
@@ -0,0 +1,65 @@
+﻿using System.Globalization;
+
+namespace System {
+	public static partial class StringierExtensions {
+		/// <summary>
+		/// Converts the string representation of a number to its 32-bit signed integer equivalent.
+		/// </summary>
+		/// <param name="Char">A char containing a number to convert.</param>
+		/// <returns>A 32-bit signed integer equivalent to the number contained in <paramref name="Char"/>.</returns>
+		public static Int32 ParseInt32(this Char Char) => Char switch
+		{
+			'0' => 0,
+			'1' => 1,
+			'2' => 2,
+			'3' => 3,
+			'4' => 4,
+			'5' => 5,
+			'6' => 6,
+			'7' => 7,
+			'8' => 8,
+			'9' => 9,
+			_ => throw new FormatException("Char is not of the correct format."),
+		};
+
+		/// <summary>
+		/// Converts the string representation of a number to its 32-bit signed integer equivalent.
+		/// </summary>
+		/// <param name="Char">A char containing a number to convert.</param>
+		/// <param name="Style">A bitwise combination of the enumeration values that indicates the style elements that can be present in <paramref name="Char"/>. A typical value to specify is <see cref="NumberStyles.AllowHexSpecifier"/>.</param>
+		/// <returns>A 32-bit signed integer equivalent to the number contained in <paramref name="Char"/>.</returns>
+		public static Int32 ParseInt32(this Char Char, NumberStyles Style) {
+			if (Style.HasFlag(NumberStyles.AllowHexSpecifier)) {
+				return Char switch
+				{
+					'0' => 0x0,
+					'1' => 0x1,
+					'2' => 0x2,
+					'3' => 0x3,
+					'4' => 0x4,
+					'5' => 0x5,
+					'6' => 0x6,
+					'7' => 0x7,
+					'8' => 0x8,
+					'9' => 0x9,
+					'a' => 0xA,
+					'A' => 0xA,
+					'b' => 0xB,
+					'B' => 0xB,
+					'c' => 0xC,
+					'C' => 0xC,
+					'd' => 0xD,
+					'D' => 0xD,
+					'e' => 0xE,
+					'E' => 0xE,
+					'f' => 0xF,
+					'F' => 0xF,
+					_ => throw new FormatException("Char is not of the correct format."),
+				};
+			}
+			else {
+				return Char.ParseInt32();
+			}
+		}
+	}
+}
