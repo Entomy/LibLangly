432ee679bb76bcbfda317ba45848cd83ac99cadc
Patrick Kelly <p-kell@live.com>
2019-11-04 19:21:59 -0500

    Implemented OccurrencesBenchmarks

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -19,23 +19,23 @@ namespace Benchmarks {
 				}),
 				new BackKeyChoice(ConsoleKey.Q, "Quit", () => Environment.Exit(0)));
 			ExtensionsChoices = new KeyChoiceSet(" Enter Choice: ",
-				new KeyChoice(ConsoleKey.D1, "Chop", () => BenchmarkRunner.Run<ChopBenchmarks>() ),
-				new KeyChoice(ConsoleKey.D2, "Clean", () => BenchmarkRunner.Run<CleanBenchmarks>() ),
+				new KeyChoice(ConsoleKey.D1, "Chop", () => BenchmarkRunner.Run<ChopBenchmarks>()),
+				new KeyChoice(ConsoleKey.D2, "Clean", () => BenchmarkRunner.Run<CleanBenchmarks>()),
 				new KeyChoice(ConsoleKey.D3, "Contains", () => {
 					_ = BenchmarkRunner.Run<ContainsCharStringBenchmarks>();
 					_ = BenchmarkRunner.Run<ContainsCharIEnumerableBenchmarks>();
 					_ = BenchmarkRunner.Run<ContainsStringIEnumerableBenchmarks>();
 				}),
-				new KeyChoice(ConsoleKey.D4, "Ensure", () => BenchmarkRunner.Run<EnsureBenchmarks>() ),
-				new KeyChoice(ConsoleKey.D5, "Join", () => BenchmarkRunner.Run<JoinBenchmarks>() ),
-				new KeyChoice(ConsoleKey.D6, "Lines", () => BenchmarkRunner.Run<LinesBenchmarks>() ),
-				new BackKeyChoice(ConsoleKey.B, "Back", () => {
-					Console.WriteChoices(MenuChoices);
-					Console.ReadChoice(MenuChoices);
-				}));
+				new KeyChoice(ConsoleKey.D4, "Ensure", () => BenchmarkRunner.Run<EnsureBenchmarks>()),
+				new KeyChoice(ConsoleKey.D5, "Join", () => BenchmarkRunner.Run<JoinBenchmarks>()),
+				new KeyChoice(ConsoleKey.D6, "Lines", () => BenchmarkRunner.Run<LinesBenchmarks>()),
+				new KeyChoice(ConsoleKey.D7, "Occurrences", () => BenchmarkRunner.Run<OccurrencesBenchmarks>()),
+				new BackKeyChoice(ConsoleKey.B, "Back", () => { }));
 
-			Console.WriteChoices(MenuChoices);
-			Console.ReadChoice(MenuChoices);
+			while (true) {
+				Console.WriteChoices(MenuChoices);
+				Console.ReadChoice(MenuChoices);
+			}
 		}
 	}
 }
