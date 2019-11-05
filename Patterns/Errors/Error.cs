08463767204b8f607cab67d17d4b1eb0660d7467
Patrick Kelly <p-kell@live.com>
2019-11-05 09:02:40 -0500

    Added a lot of comments, and did parameter name casing changes

--- Patterns/Errors/Error.cs
+++ Patterns/Errors/Error.cs
@@ -11,6 +11,7 @@ namespace Stringier.Patterns.Errors {
 	///	  * Exceptions are not always thrown, and the entire exception trace stack is remarkably heavy.
 	///	  * This still becomes an exception when needed.
 	///	* Unlike exceptions, this can be reused in-place, which means only one <see cref="Error"/> is allocated for each parse, ever.
+	///	  * Because this is a ref struct, it only ever exists on the stack, further reducing allocation burden, and cleanup burdern.
 	/// </remarks>
 	[StructLayout(LayoutKind.Explicit)]
 	internal ref struct Error {
@@ -22,56 +23,82 @@ namespace Stringier.Patterns.Errors {
 		[FieldOffset(8)] private Pattern Pattern;
 		[FieldOffset(8)] private String String;
 
+		/// <summary>
+		/// Clears out the error struct, such that no error is represented.
+		/// </summary>
 		internal void Clear() {
-			this.Code = 0x0000;
+			Code = 0x0000;
 		}
 
-		internal void Set(ErrorType Type, Char Char) {
-			this.Type = Type;
-			this.Data = ErrorData.Char;
-			this.Char = Char;
+		/// <summary>
+		/// Sets the error to the <paramref name="type"/> with the <paramref name="char"/> data source.
+		/// </summary>
+		/// <param name="type">The <see cref="ErrorType"/> to set.</param>
+		/// <param name="char">The <see cref="System.Char"/> data source.</param>
+		internal void Set(ErrorType type, Char @char) {
+			Type = type;
+			Data = ErrorData.Char;
+			Char = @char;
 		}
 
-		internal void Set(ErrorType Type, Node Node) {
-			if (Node is null) {
-				throw new ArgumentNullException(nameof(Node));
+		/// <summary>
+		/// Sets the error to the <paramref name="type"/> with the <paramref name="node"/> data source.
+		/// </summary>
+		/// <param name="type">The <see cref="ErrorType"/> to set.</param>
+		/// <param name="node">The <see cref="Patterns.Node"/> data source.</param>
+		internal void Set(ErrorType type, Node node) {
+			if (node is null) {
+				throw new ArgumentNullException(nameof(node));
 			}
-			this.Type = Type;
+			this.Type = type;
 			this.Data = ErrorData.Node;
-			this.Node = Node;
+			this.Node = node;
 		}
 
-		internal void Set(ErrorType Type, Pattern Pattern) {
-			this.Type = Type;
+		/// <summary>
+		/// Sets the error to the <paramref name="type"/> with the <paramref name="pattern"/> data source.
+		/// </summary>
+		/// <param name="type">The <see cref="ErrorType"/> to set.</param>
+		/// <param name="pattern">The <see cref="Patterns.Pattern"/> data source.</param>
+		internal void Set(ErrorType type, Pattern pattern) {
+			this.Type = type;
 			this.Data = ErrorData.Pattern;
-			this.Pattern = Pattern;
+			this.Pattern = pattern;
 		}
 
-		internal void Set(ErrorType Type, String String) {
-			if (String is null) {
-				throw new ArgumentNullException(nameof(String));
+		/// <summary>
+		/// Sets the error to the <paramref name="type"/> with the <paramref name="string"/> data source.
+		/// </summary>
+		/// <param name="type">The <see cref="ErrorType"/> to set.</param>
+		/// <param name="string">The <see cref="System.String"/> data source.</param>
+		internal void Set(ErrorType type, String @string) {
+			if (@string is null) {
+				throw new ArgumentNullException(nameof(@string));
 			}
-			this.Type = Type;
+			this.Type = type;
 			this.Data = ErrorData.String;
-			this.String = String;
+			this.String = @string;
 		}
 
+		/// <summary>
+		/// Throw the corresponding <see cref="Exception"/>, or do nothing if there is no error.
+		/// </summary>
 		public void Throw() {
-			String String;
+			String @string;
 			switch (Data) {
 			case ErrorData.None:
 				return;
 			case ErrorData.Char:
-				String = Char.ToString();
+				@string = Char.ToString();
 				break;
 			case ErrorData.Node:
-				String = Node.ToString();
+				@string = Node.ToString();
 				break;
 			case ErrorData.Pattern:
-				String = Pattern.ToString();
+				@string = Pattern.ToString();
 				break;
 			case ErrorData.String:
-				String = this.String;
+				@string = String;
 				break;
 			default:
 				throw new NotImplementedException($"{Data} doesn't have a handler.");
@@ -80,11 +107,11 @@ namespace Stringier.Patterns.Errors {
 			case ErrorType.None:
 				return;
 			case ErrorType.ConsumeFailed:
-				throw new ConsumeFailedException($"Consume failed. Expected: {String}.");
+				throw new ConsumeFailedException($"Consume failed. Expected: {@string}.");
 			case ErrorType.EndOfSource:
-				throw new EndOfSourceException($"End of source has been reached before able to parse. Expected: {String}");
+				throw new EndOfSourceException($"End of source has been reached before able to parse. Expected: {@string}");
 			case ErrorType.NeglectFailed:
-				throw new NeglectFailedException($"Neglect failed. Neglected: {String}.");
+				throw new NeglectFailedException($"Neglect failed. Neglected: {@string}.");
 			default:
 				throw new NotImplementedException($"{Type} doesn't have a conversion.");
 			}
