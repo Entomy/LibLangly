f71b0e768cf5991096f6b8a89c08b277a78f13ea
Patrick Kelly <p-kell@live.com>
2019-11-21 10:03:40 -0500

    Reworked F# extensions to use statically resolved types for better type saftey

---
+++ Extensions.FSharp/Binder.fs
@@ -0,0 +1,38 @@
+﻿namespace Stringier
+
+open System
+
+module Bindings =
+    type Binder =
+        static member Contains(source:string, value:char) = source.Contains(value)
+        static member Contains(source:string, value:string) = source.Contains(value)
+        static member Contains(source:seq<string>, value:char) = source.Contains(value)
+        static member Contains(source:seq<string>, value:string) = source.Contains(value)
+        static member Join(sequence:char[]) = sequence.Join()
+        static member Join(sequence:seq<char>) = sequence.Join()
+        static member Join(sequence:seq<string>) = sequence.Join()
+        static member Join2(sequence:seq<char>, separator:char) = sequence.Join(separator)
+        static member Join2(sequence:seq<string>, separator:char) = sequence.Join(separator)
+        static member Join2(sequence:seq<obj>, separator:char) = sequence.Join(separator)
+        static member Occurrences(source:string, value:char) = source.Occurrences(value)
+        static member Occurrences(source:string, values:char[]) = source.Occurrences(values)
+        static member Occurrences(source:seq<string>, value:char) = source.Occurrences(value)
+        static member Occurrences(source:seq<string>, values:char[]) = source.Occurrences(values)
+        static member Repeat(text:char, count:int32) = text.Repeat(count)
+        static member Repeat(text:string, count:int32) = text.Repeat(count)
+        static member Split(source:string, separator:char) = source.Split(separator)
+        static member Split(source:string, separators:char[]) = source.Split(separators)
+        static member Split(source:string, separator:string) = source.Split(separator)
+        static member Split(source:string, separators:string[]) = source.Split(separators)
+
+    let inline _contains< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Contains : ^a * ^b -> ^c)> value source = ((^t or ^a) : (static member Contains : ^a * ^b -> ^c)(source, value))
+
+    let inline _join< ^t, ^a, ^b when (^t or ^a) : (static member Join : ^a -> ^b)> sequence = ((^t or ^a) : (static member Join : ^a -> ^b)(sequence))
+
+    let inline _join2< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Join2: ^a * ^b -> ^c)> separator sequence = ((^t or ^a) : (static member Join2 : ^a * ^b -> ^c)(sequence, separator))
+
+    let inline _occurrences< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Occurrences : ^a * ^b -> ^c)> value source = ((^t or ^a) : (static member Occurrences : ^a * ^b -> ^c)(source, value))
+
+    let inline _repeat< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Repeat : ^a * ^b -> ^c)> count text = ((^t or ^a) : (static member Repeat : ^a * ^b -> ^c)(text, count))
+
+    let inline _split< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Split : ^a * ^b -> ^c)> separator source = ((^t or ^a) : (static member Split : ^a * ^b -> ^c)(source, separator))
--- Extensions.FSharp/Extensions.fs
+++ Extensions.FSharp/Extensions.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Collections.Generic
+open Bindings
 
 [<AutoOpen>]
 module Extensions =
@@ -19,13 +20,10 @@ module Extensions =
     /// </summary>
     let clean2(char:char)(source:string):string = source.Clean(char);
 
-    let contains(value:obj)(source:obj):bool =
-        match value, source with
-        | (:? char as char), (:? string as string) -> string.Contains(char)
-        | (:? char as char), (:? seq<string> as sequence) -> sequence.Contains(char)
-        | (:? string as string), (:? string as text) -> text.Contains(string)
-        | (:? string as string), (:? seq<string> as sequence) -> sequence.Contains(string)
-        | _ -> raise(ArgumentException("Not sure how to handle this combination of type arguments"))
+    /// <summary>
+    /// Tests whether the value is contained in the source
+    /// </summary>
+    let inline contains(value)(source):bool = _contains<Binder, _, _, _> value source
 
     /// <summary>
     /// Ensures the source begins with the required string, adding it if necessary.
@@ -37,29 +35,16 @@ module Extensions =
     /// </summary>
     let ensureEnds(required:string)(source:string) = source.EnsureEndsWith(required)
 
-    let inline internal ijoin< ^t, ^a, ^b when (^t or ^a) : (static member Join : ^a -> ^b)> sequence = ((^t or ^a) : (static  member Join : ^a -> ^b)(sequence))
-
     /// <summary>
     /// Joins the sequence into a string.
     /// </summary>
-    let join(sequence:obj) =
-        match sequence with
-        | :? IEnumerable<char> as charSeq -> ijoin<StringierExtensions, IEnumerable<char>, string> charSeq
-        | :? IEnumerable<string> as stringSeq -> ijoin<StringierExtensions, IEnumerable<string>, string> stringSeq
-        | _ -> raise(ArgumentException("Not sure how to join this sequence"))
-
-    let inline internal ijoin2< ^t, ^a, ^b, ^c when (^t or ^a) : (static member Join : ^a * ^b -> ^c)> separator sequence = ((^t or ^a) : (static  member Join : ^a * ^b -> ^c)(sequence, separator))
+    let inline join(sequence) = _join<Binder, _, _> sequence
 
     /// <summary>
     /// Joins the sequence into a string, interleaving the specified separator
     /// </summary>
-    let join2(separator:'a)(sequence:obj) = 
-        match sequence with
-        | :? IEnumerable<char> as charSeq -> ijoin2<StringierExtensions, _, _, string> separator charSeq
-        | :? IEnumerable<string> as stringSeq -> ijoin2<StringierExtensions, _, _, string> separator stringSeq
-        | _ -> raise(ArgumentException("Not sure how to join this sequence"))
+    let inline join2(separator)(sequence) = _join2<Binder, _, _, _> separator sequence
     
-
     /// <summary>
     /// Separate the <paramref name="String"/> into its lines.
     /// </summary>
@@ -68,13 +53,7 @@ module Extensions =
     /// <summary>
     /// Count all occurences of seek in source
     /// </summary>
-    let occurrences(seek:obj)(source:obj) =
-        match source, seek with
-        | (:? string as string), (:? char as char) -> string.Occurrences(char)
-        | (:? string as string), (:? array<char> as chars) -> string.Occurrences(chars)
-        | (:? seq<string> as strings), (:? char as char) -> strings.Occurrences(char)
-        | (:? seq<string> as strings), (:? array<char> as chars) -> strings.Occurrences(chars)
-        | _ -> raise(ArgumentException("Not sure how to count occurences for these types"))
+    let inline occurrences(seek)(source) = _occurrences<Binder, _, _, _> seek source
 
     /// <summary>
     /// Returns a new string that center-aligns the characters in this instance by padding them with spaces on both sides, for a specified total length.
@@ -89,22 +68,12 @@ module Extensions =
     /// <summary>
     /// Repeat the text, count times.
     /// </summary>
-    let repeat(count:int32)(text:obj) =
-        match text with
-        | :? char as char -> char.Repeat(count)
-        | :? string as string -> string.Repeat(count)
-        | _ -> raise(ArgumentException("Not sure how to repeat the text type; it might not be text"))
+    let inline repeat(count:int32)(text:'a):'c = _repeat<Binder, 'a, int32, 'c> count text
 
     /// <summary>
     /// Splits a string into substrings based on the separator(s).
     /// </summary>
-    let split(separator:obj)(source:string) =
-        match separator with
-        | (:? char as char) -> source.Split(char)
-        | (:? array<char> as chars) -> source.Split(chars)
-        | (:? string as string) -> source.Split(string)
-        | (:? array<string> as strings) -> source.Split(strings)
-        | _ -> raise(ArgumentException("Wasn't passed a separator type that can be used"))
+    let inline split(separator)(source) = _split<Binder, _, _, _> separator source
 
     /// <summary>
     /// Separate the string into its words.
