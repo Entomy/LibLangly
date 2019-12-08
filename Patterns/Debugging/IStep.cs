bb8dd92fa2a57a5572a6fc160db1ecda94de0da7
Patrick Kelly <p-kell@live.com>
2019-12-07 19:40:42 -0500

    Added empty IStep interface

---
+++ Patterns/Debugging/IStep.cs
@@ -0,0 +1,7 @@
+﻿namespace Stringier.Patterns.Debugging {
+	/// <summary>
+	/// Represents a <see cref="ITrace"/> step; a snapshot of execution.
+	/// </summary>
+	public interface IStep {
+	}
+}
--- Patterns/Debugging/ITrace.cs
+++ Patterns/Debugging/ITrace.cs
@@ -6,6 +6,6 @@ namespace Stringier.Patterns.Debugging {
 	/// <summary>
 	/// Represents a graph trace, a collection of the steps taken through a <see cref="Pattern"/> graph.
 	/// </summary>
-	public interface ITrace {
+	public interface ITrace : IEnumerable<IStep> {
 	}
 }
