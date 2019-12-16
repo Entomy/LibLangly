e955797f600cc8aa16360b8ec7dcb0199bfea423
Patrick Kelly <p-kell@live.com>
2019-12-15 19:32:33 -0500

    Very basic ptndbg implementation

---
+++ ptndbg/GetSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using Consolator.UI;
+using Consolator.UI.Theming;
+using Console = Consolator.Console;
+using Stringier.Patterns;
+
+namespace ptndbg {
+	public static partial class Program {
+		public static Source GetSource() {
+			Console.WriteLine("Enter <EOF> when done", ConsoleColor.DarkBlue);
+			StringBuilder sourceText = new StringBuilder();
+		GetLine:
+			String line = Console.ReadLine(" Source: ", ConsoleColor.Yellow, ConsoleColor.White);
+			if (String.Equals(line, "<EOF>", StringComparison.Ordinal)) {
+				goto Done;
+			} else {
+				_ = sourceText.Append(line);
+			}
+			goto GetLine;
+		Done:
+			return new Source(sourceText.ToString());
+		}
+	}
+}
---
+++ ptndbg/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Consolator.UI;
+using Consolator.UI.Theming;
+using Console = Consolator.Console;
+using Stringier.Patterns;
+
+namespace ptndbg {
+	public static partial class Program {
+		[SuppressMessage("Redundancy", "RCS1038:Remove empty statement.", Justification = "They are used for break-points, they don't hurt anything, stfu.")]
+		public static void Main() {
+			Theme.DefaultDark.Apply();
+
+			Source source = GetSource();
+			Pattern pattern = GetPattern();
+			;
+		}
+	}
+}
