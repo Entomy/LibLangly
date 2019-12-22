ef8a41bad6a8394d0053e095e2316d30e3e80870
Patrick Kelly <p-kell@live.com>
2019-12-22 10:58:21 -0500

    Various fixes and defensive coding

--- Patterns/Nodes/EscapedRanger.cs
+++ Patterns/Nodes/EscapedRanger.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using Stringier.Patterns.Debugging;
 using Stringier.Patterns.Errors;
 
--- Patterns/PatternFactories.cs
+++ Patterns/PatternFactories.cs
@@ -7,14 +7,30 @@ namespace Stringier.Patterns {
 		/// </summary>
 		/// <param name="delimiter">The token delimiting the start of a line comment.</param>
 		/// <returns>A pattern representing a line comment.</returns>
-		public static Pattern LineComment(String delimiter) => delimiter.Then(Many(Not(LineTerminator)));
+		public static Pattern LineComment(String delimiter) {
+			if (delimiter is null) {
+				throw new ArgumentNullException(nameof(delimiter));
+			} else if (delimiter.Length == 0) {
+				throw new ArgumentException("Line comment delimiter can not be empty", nameof(delimiter));
+			} else {
+				return delimiter.Then(Many(Not(LineTerminator)));
+			}
+		}
 
 		/// <summary>
 		/// Creates a pattern representing a string literal contained by the <paramref name="delimiter"/>.
 		/// </summary>
 		/// <param name="delimiter">The token delimiting the start and end of the literal.</param>
 		/// <returns>A pattern representing a string literal.</returns>
-		public static Pattern StringLiteral(String delimiter) => Range(delimiter, delimiter);
+		public static Pattern StringLiteral(String delimiter) {
+			if (delimiter is null) {
+				throw new ArgumentNullException(nameof(delimiter));
+			} else if (delimiter.Length == 0) {
+				throw new ArgumentException("String literal delimiter can not be empty", nameof(delimiter));
+			} else {
+				return Range(delimiter, delimiter);
+			}
+		}
 
 		/// <summary>
 		/// Creates a pattern representing a string literal contained by the <paramref name="delimiter"/>.
@@ -22,6 +38,16 @@ namespace Stringier.Patterns {
 		/// <param name="delimiter">The token delimiting the start and end of the literal.</param>
 		/// <param name="escape">The token escaping the <paramref name="delimiter"/>.</param>
 		/// <returns>A pattern representing a string literal.</returns>
-		public static Pattern StringLiteral(String delimiter, String escape) => Range(delimiter, delimiter, escape);
+		public static Pattern StringLiteral(String delimiter, String escape) {
+			if (delimiter is null || escape is null) {
+				throw new ArgumentNullException(delimiter is null ? nameof(delimiter) : nameof(escape));
+			} else if (delimiter.Length == 0) {
+				throw new ArgumentException("String literal delimiter can not be empty", nameof(delimiter));
+			} else if (escape.Length == 0) {
+				return Range(delimiter, delimiter);
+			} else {
+				return Range(delimiter, delimiter, escape);
+			}
+		}
 	}
 }
