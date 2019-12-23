d2e1c3558dab6dd5156334ee00f4dd82168d7cc0
Patrick Kelly <p-kell@live.com>
2019-12-22 20:57:32 -0500

    Fixed critical typo

--- Metrics.FSharp/EditDistance.fs
+++ Metrics.FSharp/EditDistance.fs
@@ -4,4 +4,4 @@
 module EditDistance =
     let hamming(source:string)(other:string) = Metrics.HammingDistance(source, other);
 
-    let levenshtein(source:string)(other:string) = Metrics.HammingDistance(source, other);
+    let levenshtein(source:string)(other:string) = Metrics.LevenshteinDistance(source, other);
