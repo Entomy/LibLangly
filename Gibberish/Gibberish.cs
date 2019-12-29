b39b3286746e47154dfb2f1c343c2461977dbefc
Patrick Kelly <p-kell@live.com>
2019-12-29 17:47:13 -0500

    Analyzer suppression

--- Gibberish/Gibberish.cs
+++ Gibberish/Gibberish.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace Stringier {
@@ -15,6 +16,7 @@ namespace Stringier {
 		/// <param name="reductionFactor">The factor by which to reduce the total gibberish generated. Without a reduction factor it generates a string of length <see cref="Int32.MaxValue"/>. The factor cuts this length down.</param>
 		/// <param name="bad">Whether to insert a bad character into the string.</param>
 		/// <returns>A string of gibberish.</returns>
+		[SuppressMessage("Usage", "SecurityIntelliSenseCS:MS Security rules violation", Justification = "This is just a text generator for tests and benchmarks, it doesn't need to be cryptographically secure")]
 		public static String Generate(Int32 reductionFactor, Boolean bad = false) {
 			Random random = new Random();
 			Char[] pool = new[] { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
