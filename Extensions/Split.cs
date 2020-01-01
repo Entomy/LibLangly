19fb08d1806c039a2cfbc0a11adaab0b2c8fe421
Patrick Kelly <p-kell@live.com>
2020-01-01 18:11:20 -0500

    Implemented Split() for ReadOnlySpan<Char>

--- Extensions/Split.cs
+++ Extensions/Split.cs
@@ -3,6 +3,14 @@
 namespace Stringier {
 	public static partial class StringierExtensions {
 		/// <summary>
+		/// Splits a span into substrings that are based on the characters in the separator array.
+		/// </summary>
+		/// <param name="span">The span to split.</param>
+		/// <param name="separator">A character array that delimits the substrings in this string, an empty array that contains no delimiters, or <see langword="null"/>.</param>
+		/// <returns>An array whose elements contain the substrings from this instance that are delimited by one or more characters in <paramref name="separator"/>.</returns>
+		public static String[] Split(this ReadOnlySpan<Char> span, params Char[] separator) => span.ToString().Split(separator);
+
+		/// <summary>
 		/// Splits a string into substrings based on the strings in an array.
 		/// </summary>
 		/// <param name="string">The string to split.</param>
@@ -14,5 +22,13 @@ namespace Stringier {
 			}
 			return @string.Split(separator, StringSplitOptions.None);
 		}
+
+		/// <summary>
+		/// Splits a string into substrings based on the strings in an array.
+		/// </summary>
+		/// <param name="span">The span to split.</param>
+		/// <param name="separator">A string array that delimits the substrings in this string, an empty array that contains no delimiters, or null.</param>
+		/// <returns>A string array that delimits the substrings in this string, an empty array that contains no delimiters, or null.</returns>
+		public static String[] Split(this ReadOnlySpan<Char> span, params String[] separator) => span.ToString().Split(separator);
 	}
 }
