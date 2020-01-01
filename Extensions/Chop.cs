fde2862940143a9b08ef318e18fc88e8ce13f50c
Patrick Kelly <p-kell@live.com>
2020-01-01 11:14:42 -0500

    Implemented Chop() for ReadOnlySpan<Char>

--- Extensions/Chop.cs
+++ Extensions/Chop.cs
@@ -12,17 +12,27 @@ namespace Stringier {
 			if (@string is null) {
 				throw new ArgumentNullException(nameof(@string));
 			}
+			return Chop(@string.AsSpan(), size);
+		}
+
+		/// <summary>
+		/// Chop the <paramref name="span"/> into chunks of <paramref name="size"/>
+		/// </summary>
+		/// <param name="span">Span to chop</param>
+		/// <param name="size">Size of chunks to chop into</param>
+		/// <returns>Array of chunks</returns>
+		public static String[] Chop(this ReadOnlySpan<Char> span, Int32 size) {
 			if (size <= 0) {
 				throw new ArgumentOutOfRangeException(nameof(size), "Size must be greater than 0");
-			} else if (size >= @string.Length) {
-				return new[] { @string };
+			} else if (size >= span.Length) {
+				return new[] { span.ToString() };
 			} else {
 				Int32 i = 0;
 				Int32 j = 0;
-				Int32 k = (Int32)Math.Ceiling((Double)@string.Length / size);
+				Int32 k = (Int32)Math.Ceiling((Double)span.Length / size);
 				String[] Result = new String[k];
 				while (i < k) {
-					Result[i] = (j + size) > @string.Length ? @string.Substring(j, @string.Length - j) : @string.Substring(j, size);
+					Result[i] = (j + size) > span.Length ? span.Slice(j, span.Length - j).ToString() : span.Slice(j, size).ToString();
 					i++;
 					j += size;
 				}
