17983b04c999d1145d5c9431da109db45daf31db
Patrick Kelly <p-kell@live.com>
2020-01-01 14:01:18 -0500

    Implemented Clean() for ReadOnlySpan<Char>

--- Extensions/Clean.cs
+++ Extensions/Clean.cs
@@ -13,9 +13,18 @@ namespace Stringier {
 			if (@string is null) {
 				throw new ArgumentNullException(nameof(@string));
 			}
+			return @string.AsSpan().Clean();
+		}
+
+		/// <summary>
+		/// Trim and replace multiple spaces with a single space
+		/// </summary>
+		/// <param name="span"><see cref="ReadOnlySpan{T}"/> of <see cref="Char"/> to clean</param>
+		/// <returns>Cleaned up string</returns>
+		public static String Clean(this ReadOnlySpan<Char> span) { 
 			StringBuilder Result = new StringBuilder();
 			Boolean AtSpace = false;
-			foreach (Char C in @string) {
+			foreach (Char C in span) {
 				if (Char.GetUnicodeCategory(C) == UnicodeCategory.SpaceSeparator) {
 					if (!AtSpace) {
 						AtSpace = true;
@@ -30,22 +39,32 @@ namespace Stringier {
 		}
 
 		/// <summary>
-		/// Trim and replace multiple <paramref name="Char"/> with a single <paramref name="Char"/>
+		/// Trim and replace multiple <paramref name="char"/> with a single <paramref name="char"/>
 		/// </summary>
-		/// <param name="String">String to clean</param>
-		/// <param name="Char"></param>
+		/// <param name="string">String to clean</param>
+		/// <param name="char">The character to clean</param>
 		/// <returns>Cleaned up string</returns>
-		public static String Clean(this String String, Char Char) {
-			if (String is null) {
-				throw new ArgumentNullException(nameof(String));
+		public static String Clean(this String @string, Char @char) {
+			if (@string is null) {
+				throw new ArgumentNullException(nameof(@string));
 			}
+			return @string.AsSpan().Clean(@char);
+		}
+
+		/// <summary>
+		/// Trim and replace multiple <paramref name="char"/> with a single <paramref name="char"/>
+		/// </summary>
+		/// <param name="span"><see cref="ReadOnlySpan{T}"/> of <see cref="Char"/> to clean</param>
+		/// <param name="char">The character to clean</param>
+		/// <returns>Cleaned up string</returns>
+		public static String Clean(this ReadOnlySpan<Char> span, Char @char) {
 			StringBuilder Result = new StringBuilder();
 			Boolean AtChar = false;
-			foreach (Char C in String) {
-				if (C == Char) {
+			foreach (Char C in span) {
+				if (C == @char) {
 					if (!AtChar) {
 						AtChar = true;
-						_ = Result.Append(Char);
+						_ = Result.Append(@char);
 					}
 				} else {
 					AtChar = false;
