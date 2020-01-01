a9e6ed67462e1f6927eabcc0b268a94264c9e347
Patrick Kelly <p-kell@live.com>
2020-01-01 16:43:32 -0500

    Optimized Repeat, for almost 2x speedup, and added ReadOnlySpan<Char> variant.

--- Extensions/Repeat.cs
+++ Extensions/Repeat.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 
 namespace Stringier {
 	public static partial class StringierExtensions {
@@ -26,14 +25,33 @@ namespace Stringier {
 			if (@string is null) {
 				throw new ArgumentNullException(nameof(@string));
 			}
+			Char[] result = new Char[@string.Length * count];
+			Int32 r = 0;
+			for (Int32 i = 0; i < count; i++) {
+				@string.CopyTo(0, result, r, @string.Length);
+				r += @string.Length;
+			}
+			return new String(result);
+		}
+
+		/// <summary>
+		/// Repeat the <paramref name="span"/> <paramref name="count"/> times.
+		/// </summary>
+		/// <param name="span">The <see cref="ReadOnlySpan{T}"/> of <see cref="Char"/> to repeat.</param>
+		/// <param name="count">The amount of times to repeat the <paramref name="span"/>.</param>
+		/// <returns>A <see cref="String"/> containing the repeated <paramref name="span"/>.</returns>
+		public static String Repeat(this ReadOnlySpan<Char> span, Int32 count) {
 			if (count <= 0) {
 				throw new ArgumentOutOfRangeException(nameof(count), "Count must be a positive integer");
 			}
-			StringBuilder Result = new StringBuilder(@string.Length * count);
+			String @string = span.ToString();
+			Char[] result = new Char[span.Length * count];
+			Int32 r = 0;
 			for (Int32 i = 0; i < count; i++) {
-				_ = Result.Append(@string);
+				@string.CopyTo(0, result, r, span.Length);
+				r += @string.Length;
 			}
-			return Result.ToString();
+			return new String(result);
 		}
 	}
 }
