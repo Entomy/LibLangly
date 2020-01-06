5216951154cf51ce88f830c59a4851bbdedeebe9
Patrick Kelly <p-kell@live.com>
2020-01-06 10:49:59 -0500

    Implemented Stream.Read(Int32):Byte[] extension

---
+++ Streams.Tests/StreamExtensionsTests.fs
@@ -0,0 +1,19 @@
+﻿namespace Tests.Streams
+
+open System
+open System.IO
+open Stringier.Streams
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type StreamExtensionsTests() =
+    [<TestMethod>]
+    member _.``read string`` () =
+        let stream = new StringStream("helloworld!")
+        CollectionAssert.AreEqual([|0x68uy;0x65uy;0x6Cuy;0x6Cuy;0x6Fuy|], stream.Read(5))
+
+    [<TestMethod>]
+    member _.``read file`` () =
+        let stream = new FileStream("Test.txt", FileMode.Open)
+        CollectionAssert.AreEqual([|0xEFuy;0xBBuy;0xBFuy|], stream.Read(3))
+        CollectionAssert.AreEqual([|0x68uy;0x65uy;0x6Cuy;0x6Cuy;0x6Fuy|], stream.Read(5))
---
+++ Streams/StreamExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Stringier.Streams {
+	public static class StreamExtensions {
+		/// <summary>
+		/// Reads the next, maximum of, <paramref name="count"/> characters from the input stream and advances the byte position by the amount read.
+		/// </summary>
+		/// <param name="stream"></param>
+		/// <param name="count">The maximum of the characters to read.</param>
+		/// <returns>An <see cref="Array"/> of <see cref="Byte"/> containing the read characters.</returns>
+		public static Byte[] Read(this Stream stream, Int32 count) {
+			Byte[] buf = new Byte[count];
+			Int32 c = stream.Read(buf, 0, count);
+			Byte[] res = new Byte[c];
+			Array.Copy(buf, 0, res, 0, c);
+			return res;
+		}
+	}
+}
