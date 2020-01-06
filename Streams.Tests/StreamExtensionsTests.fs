1c536e2b4002e678e8591283a81bdbff5090c82a
Patrick Kelly <p-kell@live.com>
2020-01-06 11:39:56 -0500

    Tested seeking and rereading the stream directly

--- Streams.Tests/StreamExtensionsTests.fs
+++ Streams.Tests/StreamExtensionsTests.fs
@@ -9,11 +9,27 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 type StreamExtensionsTests() =
     [<TestMethod>]
     member _.``read string`` () =
-        let stream = new StringStream("helloworld!")
+        use stream = new StringStream("helloworld!")
+        CollectionAssert.AreEqual([|0x68uy;0x65uy;0x6Cuy;0x6Cuy;0x6Fuy|], stream.Read(5))
+
+    [<TestMethod>]
+    member _.``read/seek string`` () =
+        use stream = new StringStream("helloworld!")
+        CollectionAssert.AreEqual([|0x68uy;0x65uy;0x6Cuy;0x6Cuy;0x6Fuy|], stream.Read(5))
+        stream.Position <- 0L
         CollectionAssert.AreEqual([|0x68uy;0x65uy;0x6Cuy;0x6Cuy;0x6Fuy|], stream.Read(5))
 
     [<TestMethod>]
     member _.``read file`` () =
-        let stream = new FileStream("Test.txt", FileMode.Open)
+        use stream = new FileStream("Test.txt", FileMode.Open)
+        CollectionAssert.AreEqual([|0xEFuy;0xBBuy;0xBFuy|], stream.Read(3))
+        CollectionAssert.AreEqual([|0x68uy;0x65uy;0x6Cuy;0x6Cuy;0x6Fuy|], stream.Read(5))
+
+    [<TestMethod>]
+    member _.``read/seek file`` () =
+        use stream = new FileStream("Test.txt", FileMode.Open)
+        CollectionAssert.AreEqual([|0xEFuy;0xBBuy;0xBFuy|], stream.Read(3))
+        CollectionAssert.AreEqual([|0x68uy;0x65uy;0x6Cuy;0x6Cuy;0x6Fuy|], stream.Read(5))
+        stream.Position <- 0L
         CollectionAssert.AreEqual([|0xEFuy;0xBBuy;0xBFuy|], stream.Read(3))
         CollectionAssert.AreEqual([|0x68uy;0x65uy;0x6Cuy;0x6Cuy;0x6Fuy|], stream.Read(5))
