78891eb16ed4e36090f55487cb4fbcc9e0fb06a6
Patrick Kelly <p-kell@live.com>
2020-01-09 17:37:54 -0500

    Added an Info() extension method to help with discovery. StringInfo is very useful but a lot of people don't know it even exists because of where it's located.

---
+++ Extensions/Info.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Stringier {
+	public static partial class StringierExtensions {
+		/// <summary>
+		/// Gets the <see cref="StringInfo"/> object for this <paramref name="string"/>.
+		/// </summary>
+		/// <param name="string">The source <see cref="String"/>.</param>
+		/// <returns>A <see cref="StringInfo"/> object.</returns>
+		public static StringInfo Info(this String @string) => new StringInfo(@string);
+	}
+}
