b03d048d8876cfc99b6a7215c9e5f7bcbcb15136
Patrick Kelly <p-kell@live.com>
2020-01-14 11:31:56 -0500

    Implemented ToStrings() for array conversion

---
+++ Extensions.Tests/ToStringsTests.fs
@@ -0,0 +1,12 @@
+﻿namespace Tests.Extensions
+
+open System
+open Stringier
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type ToStringsTests() =
+    [<TestMethod>]
+    member _.``to strings`` () =
+        CollectionAssert.AreEqual([|""|], [|null|].ToStrings(String.Empty))
+        CollectionAssert.AreEqual([|"1";"2";"3"|], [|1;2;3|].ToStrings())
---
+++ Extensions/ToStrings.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Stringier {
+	public static partial class StringierExtensions {
+		/// <summary>
+		/// Converts each member of <paramref name="array"/> to its string representation.
+		/// </summary>
+		/// <typeparam name="T">The type of the members of <paramref name="array"/>.</typeparam>
+		/// <param name="array">An <see cref="Array"/> of elements to convert.</param>
+		/// <returns>An <see cref="Array"/> of <see cref="String"/> with each member of <paramref name="array"/> converted to its string representation.</returns>
+		public static String[] ToStrings<T>(this T[] array) where T : struct {
+			if (array is null) {
+				throw new ArgumentNullException(nameof(array));
+			}
+			String[] result = new String[array.Length];
+			for (Int32 i = 0; i < array.Length; i++) {
+				result[i] = array[i].ToString();
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// Converts each member of <paramref name="array"/> to its string representation.
+		/// </summary>
+		/// <typeparam name="T">The type of the members of <paramref name="array"/>.</typeparam>
+		/// <param name="array">An <see cref="Array"/> of elements to convert.</param>
+		/// <param name="nullReplacement">The <see cref="String"/> to use instead, if a member is <see langword="null"/>.</param>
+		/// <returns>An <see cref="Array"/> of <see cref="String"/> with each member of <paramref name="array"/> converted to its string representation.</returns>
+		public static String[] ToStrings<T>(this T?[] array, String nullReplacement) where T : struct {
+			if (array is null) {
+				throw new ArgumentNullException(nameof(array));
+			}
+			String[] result = new String[array.Length];
+			for (Int32 i = 0; i < array.Length; i++) {
+				result[i] = array[i]?.ToString() ?? nullReplacement;
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// Converts each member of <paramref name="array"/> to its string representation.
+		/// </summary>
+		/// <typeparam name="T">The type of the members of <paramref name="array"/>.</typeparam>
+		/// <param name="array">An <see cref="Array"/> of elements to convert.</param>
+		/// <param name="nullReplacement">The <see cref="String"/> to use instead, if a member is <see langword="null"/>.</param>
+		/// <returns>An <see cref="Array"/> of <see cref="String"/> with each member of <paramref name="array"/> converted to its string representation.</returns>
+		public static String[] ToStrings<T>(this T?[] array, String nullReplacement) where T : class {
+			if (array is null) {
+				throw new ArgumentNullException(nameof(array));
+			}
+			String[] result = new String[array.Length];
+			for (Int32 i = 0; i < array.Length; i++) {
+				result[i] = array[i]?.ToString() ?? nullReplacement;
+			}
+			return result;
+		}
+	}
+}
