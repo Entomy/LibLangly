8a0486d32b4fe040d9538d56b7b6f565bd5df109
Patrick Kelly <p-kell@live.com>
2020-01-14 10:39:47 -0500

    Implemented FormatTable with formatting functions. Closes #24

---
+++ Extensions.Tests/FormatTableTests.fs
@@ -0,0 +1,25 @@
+﻿namespace Tests.Extensions
+
+open System
+open Stringier
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type FormatTableTests() =
+    [<TestMethod>]
+    member _.``format table`` () =
+        let mutable table = FormatTable(5, Alignment.Left)
+        table.Format("one", "two", "three", "four", "five")
+        Assert.AreEqual("one│two│three│four│five\n", table.ToString())
+        table.Format("six", "seven", "eight", "nine", "ten")
+        Assert.AreEqual("one│two  │three│four│five\nsix│seven│eight│nine│ten \n", table.ToString())
+        table <- FormatTable(5, Alignment.Right)
+        table.Format("one", "two", "three", "four", "five")
+        Assert.AreEqual("one│two│three│four│five\n", table.ToString())
+        table.Format("six", "seven", "eight", "nine", "ten")
+        Assert.AreEqual("one│  two│three│four│five\nsix│seven│eight│nine│ ten\n", table.ToString())
+        table <- FormatTable(5, Alignment.Center)
+        table.Format("one", "two", "three", "four", "five")
+        Assert.AreEqual("one│two│three│four│five\n", table.ToString())
+        table.Format("six", "seven", "eight", "nine", "ten")
+        Assert.AreEqual("one│ two │three│four│five\nsix│seven│eight│nine│ten \n", table.ToString())
