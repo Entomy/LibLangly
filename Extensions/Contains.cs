c1eb9946729aef186dd383b053965171061644b5
Patrick Kelly <p-kell@live.com>
2020-01-14 16:09:25 -0500

    A few doc fixes

--- Extensions/Contains.cs
+++ Extensions/Contains.cs
@@ -9,7 +9,7 @@ namespace Stringier {
 		/// </summary>
 		/// <param name="string">The string to check.</param>
 		/// <param name="value">The char to seek.</param>
-		/// <returns>true if the value parameter occurs within this string, or if value is the empty char (''); otherwise, false.</returns>
+		/// <returns><see langword="true"/> if the value parameter occurs within this string; otherwise, <see langword="false"/>.</returns>
 		public static Boolean Contains(this String @string, Char value) {
 			if (@string is null) {
 				throw new ArgumentNullException(nameof(@string));
@@ -24,11 +24,11 @@ namespace Stringier {
 
 #if NETCOREAPP3_0 || NETCOREAPP3_1
 		/// <summary>
-		/// Returns a value indicating whether a specified char occurs within this string.
+		/// Returns a value indicating whether a specified rune occurs within this string.
 		/// </summary>
 		/// <param name="string">The string to check.</param>
 		/// <param name="value">The rune to seek.</param>
-		/// <returns>true if the value parameter occurs within this string, or if value is the empty char (''); otherwise, false.</returns>
+		/// <returns><see langword="true"/> if the value parameter occurs within this string; otherwise, <see langword="false"/>.</returns>
 		public static Boolean Contains(this String @string, Rune value) {
 			if (@string is null) {
 				throw new ArgumentNullException(nameof(@string));
@@ -47,7 +47,7 @@ namespace Stringier {
 		/// </summary>
 		/// <param name="strings">The enumeration of strings to check.</param>
 		/// <param name="value">The char to seek.</param>
-		/// <returns>true if the value parameter occurs within any of the strings, or if the value is the empty char (''); otherwise, false.</returns>
+		/// <returns><see langword="true"/> if the value parameter occurs within any of the strings; otherwise, <see langword="false"/>.</returns>
 		public static Boolean Contains(this IEnumerable<String> strings, Char value) {
 			if (strings is null) {
 				throw new ArgumentNullException(nameof(strings));
@@ -66,7 +66,7 @@ namespace Stringier {
 		/// </summary>
 		/// <param name="strings">The enumeration of strings to check.</param>
 		/// <param name="value">The rune to seek.</param>
-		/// <returns>true if the value parameter occurs within any of the strings, or if the value is the empty char (''); otherwise, false.</returns>
+		/// <returns><see langword="true"/> if the value parameter occurs within any of the strings; otherwise, <see langword="false"/>.</returns>
 		public static Boolean Contains(this IEnumerable<String> strings, Rune value) {
 			if (strings is null) {
 				throw new ArgumentNullException(nameof(strings));
@@ -85,7 +85,7 @@ namespace Stringier {
 		/// </summary>
 		/// <param name="strings">The enumeration of strings to check.</param>
 		/// <param name="value">The string to seek.</param>
-		/// <returns>true if the value parameter occurs within any of the strings, or if the value is the empty string (""); otherwise, false.</returns>
+		/// <returns><see langword="true"/> if the value parameter occurs within any of the strings; otherwise, <see langword="false"/>.</returns>
 		public static Boolean Contains(this IEnumerable<String> strings, String value) {
 			if (strings is null || value is null) {
 				throw new ArgumentNullException(strings is null ? nameof(strings) : nameof(value));
