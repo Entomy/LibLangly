be2d09837094f6229ac2b294c371e532d1fb4c39
Patrick Kelly <p-kell@live.com>
2020-01-29 07:51:49 -0500

    More tests

--- Core.Tests/SurrogatePairTests.fs
+++ Core.Tests/SurrogatePairTests.fs
@@ -9,16 +9,30 @@ type SurrogatePairTests() =
     [<DataTestMethod>]
     [<DataRow(0xD800u, 0xDC00u)>]
     [<DataRow(0xDBFFu, 0xDFFFu)>]
-    member _.``constructor high-low`` (high:uint32, low:uint32) =
+    member _.``constructor high-low - valid`` (high:uint32, low:uint32) = 
         SurrogatePair(CodePoint(high), CodePoint(low))
         ()
 
     [<DataTestMethod>]
+    [<DataRow(0xD800u, 0xD800u)>]
+    [<DataRow(0xDFFFu, 0xDFFFu)>]
+    [<DataRow(0xDE00u, 0xDB00u)>]
+    [<DataRow(0xD900u, 0x1234u)>]
+    [<DataRow(0xD900u, 0xE000u)>]
+    [<DataRow(0x1234u, 0xDE00u)>]
+    [<DataRow(0xDC00u, 0xDE00u)>]
+    member _.``constructor high-low - invalid`` (high:uint32, low:uint32) =
+        Assert.ThrowsException<ArgumentOutOfRangeException>((fun () -> SurrogatePair(CodePoint(high), CodePoint(low)).CodePoint.IsAscii |> ignore)) |> ignore
+
+    [<DataTestMethod>]
     [<DataRow(0x10330u)>]
-    member _.``constructor SMP`` (value:uint32) =
+    member _.``constructor SMP - valid`` (value:uint32) =
         SurrogatePair(CodePoint(value))
         ()
 
     [<DataTestMethod>]
+    [<DataRow(0x010000u, 0xD800u, 0xDC00u)>]
     [<DataRow(0x01D11Eu, 0xD834u, 0xDD1Eu)>]
+    [<DataRow(0x01F3A8u, 0xD83Cu, 0xDFA8u)>]
+    [<DataRow(0x10FFFFu, 0xDBFFu, 0xDFFFu)>]
     member _.``to codepoint`` (exp:uint32, high:uint32, low:uint32) = Assert.IsTrue(CodePoint(exp).Equals(SurrogatePair(CodePoint(high), CodePoint(low)).CodePoint))
