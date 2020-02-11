3522f33dea326bc379ebeda9f91c54da2e244737
Patrick Kelly <p-kell@live.com>
2020-02-10 21:14:10 -0500

    Added ToLower/ToUpper and variant extensions for Glyph

--- Extensions/ToUpper.cs
+++ Extensions/ToUpper.cs
@@ -20,6 +20,13 @@ namespace Stringier {
 		public static Rune ToUpper(this Rune rune) => Rune.ToUpper(rune, CultureInfo.CurrentCulture);
 
 		/// <summary>
+		/// Converts this <see cref="Glyph"/> to its uppercase equivalent.
+		/// </summary>
+		/// <param name="glyph">The grapheme to convert.</param>
+		/// <returns>The uppercase equivalent of <paramref name="glyph"/>, or the unchanged value of <paramref name="glyph"/> if <paramref name="glyph"/> is already uppercase, has no uppercase equivalent, or is not alphabetic.</returns>
+		public static Glyph ToUpper(this Glyph glyph) => Glyph.ToUpper(glyph);
+
+		/// <summary>
 		/// Converts this <see cref="Char"/> to its uppercase equivalent using specified culture-specific formatting information.
 		/// </summary>
 		/// <param name="char">The Unicode character to convert.</param>
@@ -42,6 +49,17 @@ namespace Stringier {
 		}
 
 		/// <summary>
+		/// Converts this <see cref="Glyph"/> to its uppercase equivalent using specified culture-specific formatting information.
+		/// </summary>
+		/// <param name="glyph">The grapheme to convert.</param>
+		/// <param name="culture">An object that supplies culture-specific casing rules.</param>
+		/// <returns>The uppercase equivalent of <paramref name="glyph"/>, modified according to <paramref name="culture"/>, or the unchanged value of <paramref name="glyph"/>, if <paramref name="glyph"/> is already uppercase or not alphabetic.</returns>
+		public static Glyph ToUpper(this Glyph glyph, CultureInfo culture) {
+			Guard.NotNull(culture, nameof(culture));
+			return Glyph.ToUpper(glyph, culture);
+		}
+
+		/// <summary>
 		/// Converts this <see cref="Char"/> to its uppercase equivalent using the casing rules of the invariant culture.
 		/// </summary>
 		/// <param name="char">The Unicode character to convert.</param>
@@ -54,5 +72,12 @@ namespace Stringier {
 		/// <param name="rune">The Unicode character to convert.</param>
 		/// <returns>The uppercase equivalent of the <paramref name="rune"/> parameter, or the unchanged value of <paramref name="rune"/>, if <paramref name="rune"/> is already uppercase or not alphabetic.</returns>
 		public static Rune ToUpperInvariant(this Rune rune) => Rune.ToUpperInvariant(rune);
+
+		/// <summary>
+		/// Converts this <see cref="Glyph"/> to its uppercase equivalent using the casing rules of the invariant culture.
+		/// </summary>
+		/// <param name="glyph">The grapheme to convert.</param>
+		/// <returns>The uppercase equivalent of the <paramref name="glyph"/> parameter, or the unchanged value of <paramref name="glyph"/>, if <paramref name="glyph"/> is already uppercase or not alphabetic.</returns>
+		public static Glyph ToUpperInvariant(this Glyph glyph) => Glyph.ToUpperInvariant(glyph);
 	}
 }
