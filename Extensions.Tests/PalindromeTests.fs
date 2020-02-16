d8c6a506976823387c31e4dffc905a0de5e18415
Patrick Kelly <p-kell@live.com>
2020-02-15 19:07:32 -0500

    Implemented grapheme-wise palindrome checker, as well as expanded char-wise checkers to cover char[] and span<char> now as well. Closes #56

--- Extensions.Tests/PalindromeTests.fs
+++ Extensions.Tests/PalindromeTests.fs
@@ -7,6 +7,8 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 [<TestClass>]
 type PalindromeTests() =
     [<DataTestMethod>]
+    [<DataRow("")>]
+    [<DataRow("a")>]
     [<DataRow("detartrated")>]
     [<DataRow("tattarrattat")>]
     [<DataRow("Malayalam")>]
@@ -15,5 +17,12 @@ type PalindromeTests() =
     [<DataRow("Able was I ere I saw Elba")>]
     [<DataRow("A man, a plan, a canal, Panama!")>]
     [<DataRow("Do, O God, no evil deed! Live on! Do good!")>]
-    member _.``palindrome`` (src:string) =
-        Assert.IsTrue(palindrome src)
+    [<DataRow("Café Éfac")>] // This uses combining marks
+    member _.``palindrome - string`` (src:string) = Assert.IsTrue(palindrome src)
+
+    [<DataTestMethod>]
+    [<DataRow([|'a'|])>]
+    [<DataRow([|'a';'a'|])>]
+    [<DataRow([|'á';'a';'á'|])>]
+    [<DataRow([|'á';'.';'a';'á';'.'|])>]
+    member _.``palindrome - chars`` (src:char[]) = Assert.IsTrue(palindrome src)
--- Extensions/IsPalindrome.cs
+++ Extensions/IsPalindrome.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using Defender;
 
 namespace Stringier {
 	public static partial class StringierExtensions {
@@ -8,7 +8,38 @@ namespace Stringier {
 		/// </summary>
 		/// <param name="string">The <see cref="String"/> to check.</param>
 		/// <returns><see langword="true"/> if <paramref name="string"/> is a palindrome; otherwise, <see langword="false"/>.</returns>
-		public static Boolean IsPalindrome(this String @string) => !(@string is null) && @string.AsSpan().IsPalindrome();
+		public static Boolean IsPalindrome(this String @string) {
+			Guard.NotNull(@string, nameof(@string));
+			String prepped = PalindromeStrip(@string);
+			String reversed = prepped.Reverse();
+			return String.Equals(prepped, reversed, StringComparison.CurrentCultureIgnoreCase);
+		}
+
+		/// <summary>
+		/// Checks if the <paramref name="chars"/> is a palindrome.
+		/// </summary>
+		/// <param name="chars">The <see cref="Array"/> of <see cref="Char"/> to check.</param>
+		/// <returns><see langword="true"/> if <paramref name="chars"/> is a palindrome; otherwise, <see langword="false"/>.</returns>
+		public static Boolean IsPalindrome(this Char[] chars) => chars.AsSpan().IsPalindrome();
+
+		/// <summary>
+		/// Checks if the <paramref name="span"/> is a palindrome.
+		/// </summary>
+		/// <param name="span">The <see cref="ReadOnlySpan{T}"/> of <see cref="Char"/> to check.</param>
+		/// <returns><see langword="true"/> if <paramref name="span"/> is a palindrome; otherwise, <see langword="false"/>.</returns>
+		public static Boolean IsPalindrome(this Span<Char> span) {
+			ReadOnlySpan<Char> prepped = PalindromeStrip(span);
+			Span<Char> reversed = new Char[prepped.Length];
+			prepped.CopyTo(reversed);
+			reversed.Reverse();
+			// Now actually check it's a palindrome
+			for (Int32 i = 0; i < prepped.Length; i++) {
+				if (prepped[i] != reversed[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
 
 		/// <summary>
 		/// Checks if the <paramref name="span"/> is a palindrome.
@@ -16,6 +47,32 @@ namespace Stringier {
 		/// <param name="span">The <see cref="ReadOnlySpan{T}"/> of <see cref="Char"/> to check.</param>
 		/// <returns><see langword="true"/> if <paramref name="span"/> is a palindrome; otherwise, <see langword="false"/>.</returns>
 		public static Boolean IsPalindrome(this ReadOnlySpan<Char> span) {
+			ReadOnlySpan<Char> prepped = PalindromeStrip(span);
+			Span<Char> reversed = new Char[prepped.Length];
+			prepped.CopyTo(reversed);
+			reversed.Reverse();
+			// Now actually check it's a palindrome
+			for (Int32 i = 0; i < prepped.Length; i++) {
+				if (prepped[i] != reversed[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private static String PalindromeStrip(String @string) {
+			// First we need to build the string without any punctuation or whitespace or any other unrelated-to-reading characters
+			Char[] builder = new Char[@string.Length];
+			Int32 b = 0;
+			foreach (Char s in @string) {
+				if (!(s.IsControl() || s.IsPunctuation() || s.IsSeparator() || s.IsSymbol() || s.IsWhiteSpace())) {
+					builder[b++] = s;
+				}
+			}
+			return new String(builder, 0, b);
+		}
+
+		private static ReadOnlySpan<Char> PalindromeStrip(ReadOnlySpan<Char> span) {
 			// First we need to build the string without any punctuation or whitespace or any other unrelated-to-reading characters
 			Char[] builder = new Char[span.Length];
 			Int32 b = 0;
@@ -24,10 +81,7 @@ namespace Stringier {
 					builder[b++] = s;
 				}
 			}
-			String prepped = new String(builder, 0, b);
-			String reversed = prepped.Reverse().Join();
-			// Now actually check it's a palindrome
-			return String.Equals(prepped, reversed, StringComparison.CurrentCultureIgnoreCase);
+			return builder.AsSpan().Slice(0, b);
 		}
 	}
 }
