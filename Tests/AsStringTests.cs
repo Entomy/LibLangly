f0fbd6785de7d08fd39bc19d182b24c37c0fdd7e
Patrick Kelly <p-kell@live.com>
2020-02-29 14:48:04 -0500

    Readded ToStrings tests

--- Tests/AsStringTests.cs
+++ Tests/AsStringTests.cs
@@ -4,7 +4,7 @@ using Stringier;
 using Xunit;
 
 namespace Tests {
-	public class AsStringsTests {
+	public class AsStringTests {
 		[Theory]
 		[InlineData("hello", new[] { 0x68, 0x65, 0x6C, 0x6C, 0x6F })]
 		[InlineData("привет", new[] { 0x43F, 0x440, 0x438, 0x432, 0x435, 0x442 })]
