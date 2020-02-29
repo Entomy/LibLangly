9f9a05da300eb1b4bb7ab01f16b15680052e83f3
Patrick Kelly <p-kell@live.com>
2020-02-29 12:10:05 -0500

    Readded Ensure tests

---
+++ Tests/EnsureTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Stringier;
+using Xunit;
+
+namespace Tests {
+	public class EnsureTests {
+		[Theory]
+		[InlineData("Bob Saget", "Mr. ", "Mr. Bob Saget")]
+		[InlineData("Mr. Bob Saget", "Mr. ", "Mr. Bob Saget")]
+		public void EnsureBeginsWith(String source, String required, String expected) => Assert.Equal(expected, source.EnsureBeginsWith(required));
+
+		[Theory]
+		[InlineData("Gregory House", " MD", "Gregory House MD")]
+		[InlineData("Gregory House MD", " MD", "Gregory House MD")]
+		public void EnsureEndsWith(String source, String required, String expected) => Assert.Equal(expected, source.EnsureEndsWith(required));
+	}
+}
