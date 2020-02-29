030f084c699064a27d0921274e69a9cffcda4781
Patrick Kelly <p-kell@live.com>
2020-02-29 12:18:36 -0500

    Readded Equals tests

---
+++ Tests/EqualsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Stringier;
+using Xunit;
+
+namespace Tests {
+	public class EqualsTests {
+		[Theory]
+		[InlineData("", "", true)]
+		[InlineData("", "hello", false)]
+		[InlineData("hello", "", false)]
+		[InlineData("hello", "hello", true)]
+		public void Equals_Mixed(String source, String other, Boolean expected) {
+			Assert.Equal(expected, source.Equals(other.AsSpan(), StringComparison.CurrentCulture));
+			Assert.Equal(expected, source.AsSpan().Equals(other, StringComparison.CurrentCulture));
+		}
+	}
+}
