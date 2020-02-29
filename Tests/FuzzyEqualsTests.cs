3eba482a8686fdedeab86df4389af881a88b0b7d
Patrick Kelly <p-kell@live.com>
2020-02-29 12:46:28 -0500

    Readded FuzzyEquals tests

---
+++ Tests/FuzzyEqualsTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Stringier;
+using Xunit;
+
+namespace Tests {
+	public class FuzzyEqualsTests {
+		[Theory]
+		[InlineData("bob", "bob", true)]
+		[InlineData("bob", "mob", true)]
+		[InlineData("bob", "mom", false)]
+		public void FuzzyEquals(String source, String other, Boolean expected) => Assert.Equal(expected, source.FuzzyEquals(other));
+	}
+}
