6c8b3437218c81bf0351e63b71f3b42bf4145c0b
Patrick Kelly <p-kell@live.com>
2020-02-29 13:42:14 -0500

    Readded Repeat tests

---
+++ Tests/RepeatTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Stringier;
+using Xunit;
+
+namespace Tests {
+	public class RepeatTests {
+		[Theory]
+		[InlineData('a', 3, "aaa")]
+		public void Repeat_Char(Char source, Int32 count, String expected) => Assert.Equal(expected, source.Repeat(count));
+
+		[Theory]
+		[InlineData("hi!", 3, "hi!hi!hi!")]
+		public void Repeat_String(String source, Int32 count, String expected) => Assert.Equal(expected, source.Repeat(count));
+	}
+}
