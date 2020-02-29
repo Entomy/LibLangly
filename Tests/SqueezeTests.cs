f2efc5682d2eacea993c97b1efa7d4d99f530c09
Patrick Kelly <p-kell@live.com>
2020-02-29 14:28:39 -0500

    Readded Squeeze tests

---
+++ Tests/SqueezeTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Stringier;
+using Xunit;
+
+namespace Tests {
+	public class SqueezeTests {
+		[Theory]
+		[InlineData("hello", "helo")]
+		[InlineData("Leeeeeeeerooooooooooyyyyyyyy Jeeeeeeeenkiiiiiiiiins", "Leroy Jenkins")]
+		public void Squeeze(String source, String expected) => Assert.Equal(expected, source.Squeeze());
+	}
+}
