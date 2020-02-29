72019564ad691d8ff30a999418c45e060d254a56
Patrick Kelly <p-kell@live.com>
2020-02-29 14:51:37 -0500

    Readded Words tests

---
+++ Tests/WordsTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Stringier;
+using Xunit;
+
+namespace Tests {
+	public class WordsTests {
+		[Theory]
+		[InlineData("", new[] { "" })]
+		[InlineData("Hello world", new[] { "Hello", "world" })]
+		public void Words(String source, String[] expected) => Assert.Equal(expected, source.Words());
+	}
+}
