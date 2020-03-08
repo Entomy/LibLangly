b9804d1fb371063a621cea5e633b7a3bd205d967
Patrick Kelly <p-kell@live.com>
2020-03-07 19:54:30 -0500

    Added Equals() overloads involving Rune

--- Core/Extensions/Equals.cs
+++ Core/Extensions/Equals.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Text;
 
 namespace Stringier {
 	public static partial class StringierExtensions {
@@ -24,6 +25,74 @@ namespace Stringier {
 				return @char.Equals(other);
 			}
 		}
+
+		/// <summary>
+		/// Determines whether this <see cref="Char"/> and a specified <see cref="Rune"/> object have the same value. A parameter specifies the culture, case, and sort rules used in the comparison.
+		/// </summary>
+		/// <remarks>
+		/// Note that because this compares individual characters, a lot of cultural rules do not apply due to multi-char equivalences. This should only be considered to check the cultural casing rules, nothing more.
+		/// </remarks>
+		/// <param name="char">The <see cref="Char"/> instance.</param>
+		/// <param name="other">The <see cref="Rune"/> to compare to this instance.</param>
+		/// <returns><see langword="true"/> if the value of the <paramref name="other"/> parameter is the same as this char; otherwise, <see langword="false"/>.</returns>
+		public static Boolean Equals(this Char @char, Rune other) => new Rune(@char).Equals(other, StringComparison.CurrentCulture);
+
+		/// <summary>
+		/// Determines whether this <see cref="Char"/> and a specified <see cref="Rune"/> object have the same value. A parameter specifies the culture, case, and sort rules used in the comparison.
+		/// </summary>
+		/// <remarks>
+		/// Note that because this compares individual characters, a lot of cultural rules do not apply due to multi-char equivalences. This should only be considered to check the cultural casing rules, nothing more.
+		/// </remarks>
+		/// <param name="char">The <see cref="Char"/> instance.</param>
+		/// <param name="other">The <see cref="Rune"/> to compare to this instance.</param>
+		/// <param name="comparisonType">One of the enumeration values that specifies how the chars will be compared.</param>
+		/// <returns><see langword="true"/> if the value of the <paramref name="other"/> parameter is the same as this char; otherwise, <see langword="false"/>.</returns>
+		public static Boolean Equals(this Char @char, Rune other, StringComparison comparisonType) => new Rune(@char).Equals(other, comparisonType);
+
+		/// <summary>
+		/// Determines whether this <see cref="Rune"/> and a specified <see cref="Rune"/> object have the same value. A parameter specifies the culture, case, and sort rules used in the comparison.
+		/// </summary>
+		/// <remarks>
+		/// Note that because this compares individual characters, a lot of cultural rules do not apply due to multi-char equivalences. This should only be considered to check the cultural casing rules, nothing more.
+		/// </remarks>
+		/// <param name="rune">The <see cref="Rune"/> instance.</param>
+		/// <param name="other">The <see cref="Rune"/> to compare to this instance.</param>
+		/// <param name="comparisonType">One of the enumeration values that specifies how the chars will be compared.</param>
+		/// <returns><see langword="true"/> if the value of the <paramref name="other"/> parameter is the same as this char; otherwise, <see langword="false"/>.</returns>
+		public static Boolean Equals(this Rune rune, Rune other, StringComparison comparisonType) {
+			switch (comparisonType) {
+			case StringComparison.OrdinalIgnoreCase:
+			case StringComparison.InvariantCultureIgnoreCase:
+				return rune.ToUpperInvariant().Equals(other.ToUpperInvariant());
+			case StringComparison.CurrentCultureIgnoreCase:
+				return rune.ToUpper(CultureInfo.CurrentCulture).Equals(other.ToUpper(CultureInfo.CurrentCulture));
+			default:
+				return rune.Equals(other);
+			}
+		}
+
+		/// <summary>
+		/// Determines whether this <see cref="Rune"/> and a specified <see cref="Char"/> object have the same value. A parameter specifies the culture, case, and sort rules used in the comparison.
+		/// </summary>
+		/// <remarks>
+		/// Note that because this compares individual characters, a lot of cultural rules do not apply due to multi-char equivalences. This should only be considered to check the cultural casing rules, nothing more.
+		/// </remarks>
+		/// <param name="rune">The <see cref="Rune"/> instance.</param>
+		/// <param name="other">The <see cref="Char"/> to compare to this instance.</param>
+		/// <returns><see langword="true"/> if the value of the <paramref name="other"/> parameter is the same as this char; otherwise, <see langword="false"/>.</returns>
+		public static Boolean Equals(this Rune rune, Char other) => rune.Equals(new Rune(other), StringComparison.CurrentCulture);
+
+		/// <summary>
+		/// Determines whether this <see cref="Rune"/> and a specified <see cref="Char"/> object have the same value. A parameter specifies the culture, case, and sort rules used in the comparison.
+		/// </summary>
+		/// <remarks>
+		/// Note that because this compares individual characters, a lot of cultural rules do not apply due to multi-char equivalences. This should only be considered to check the cultural casing rules, nothing more.
+		/// </remarks>
+		/// <param name="rune">The <see cref="Rune"/> instance.</param>
+		/// <param name="other">The <see cref="Char"/> to compare to this instance.</param>
+		/// <param name="comparisonType">One of the enumeration values that specifies how the chars will be compared.</param>
+		/// <returns><see langword="true"/> if the value of the <paramref name="other"/> parameter is the same as this char; otherwise, <see langword="false"/>.</returns>
+		public static Boolean Equals(this Rune rune, Char other, StringComparison comparisonType) => rune.Equals(new Rune(other), comparisonType);
 
 		/// <summary>
 		/// Determines whether this instance and the specified <see cref="ReadOnlySpan{T}"/> of <see cref="Char"/> object have the same value.
