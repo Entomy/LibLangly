08452bf02e376e3f18a6e6bb8d4b574efe995ba7
Patrick Kelly <p-kell@live.com>
2020-03-08 09:29:26 -0400

    Fixed IsCombiningMark, and added more specific classifications for combining marks

--- Core/Extensions/IsCombiningMark.cs
+++ Core/Extensions/IsCombiningMark.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text;
 
 namespace Stringier {
@@ -8,23 +9,31 @@ namespace Stringier {
 		/// </summary>
 		/// <param name="char">The Unicode character to evaluate.</param>
 		/// <returns><see langword="true"/> if <paramref name="char"/> is a combining mark; otherwise, <see langword="false"/>.</returns>
-		public static Boolean IsCombiningMark(this Char @char) => 0x0300 <= @char && @char <= 0x036F
-			|| 0x1AB0 <= @char && @char <= 0x1ABE
-			|| 0x1DC0 <= @char && @char <= 0x1DF9
-			|| 0x1DFB <= @char && @char <= 0x1DFF
-			|| 0x20D0 <= @char && @char <= 0x20f0
-			|| 0xFE20 <= @char && @char <= 0xFE2F;
+		public static Boolean IsCombiningMark(this Char @char) {
+			switch (@char.GetUnicodeCategory()) {
+			case UnicodeCategory.NonSpacingMark:
+			case UnicodeCategory.SpacingCombiningMark:
+			case UnicodeCategory.EnclosingMark:
+				return true;
+			default:
+				return false;
+			}
+		}
 
 		/// <summary>
 		/// Indicates whether this <see cref="Rune"/> is categorized as a combining mark.
 		/// </summary>
 		/// <param name="rune">The Unicode character to evaluate.</param>
 		/// <returns><see langword="true"/> if <paramref name="rune"/> is a combining mark; otherwise, <see langword="false"/>.</returns>
-		public static Boolean IsCombiningMark(this Rune rune) => 0x0300 <= rune.Value && rune.Value <= 0x036F
-			|| 0x1AB0 <= rune.Value && rune.Value <= 0x1ABE
-			|| 0x1DC0 <= rune.Value && rune.Value <= 0x1DF9
-			|| 0x1DFB <= rune.Value && rune.Value <= 0x1DFF
-			|| 0x20D0 <= rune.Value && rune.Value <= 0x20f0
-			|| 0xFE20 <= rune.Value && rune.Value <= 0xFE2F;
+		public static Boolean IsCombiningMark(this Rune rune) {
+			switch (rune.GetUnicodeCategory()) {
+			case UnicodeCategory.NonSpacingMark:
+			case UnicodeCategory.SpacingCombiningMark:
+			case UnicodeCategory.EnclosingMark:
+				return true;
+			default:
+				return false;
+			}
+		}
 	}
 }
