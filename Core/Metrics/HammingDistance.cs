dbbd7a5fc8060e9cabf01d2483c5a4f26a5ee878
Patrick Kelly <p-kell@live.com>
2020-03-07 20:02:41 -0500

    Reorganized a bit, made Unsafe internal again

--- Core/Metrics/HammingDistance.cs
+++ Core/Metrics/HammingDistance.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using Defender;
 
 namespace Stringier {
@@ -13,7 +14,7 @@ namespace Stringier {
 			Guard.NotNull(source, nameof(source));
 			Guard.NotNull(other, nameof(other));
 			Guard.Equal(source.Length, nameof(source), other.Length, nameof(other));
-			return ReferenceEquals(source, other) ? 0 : Unsafe.HammingDistance(source.AsSpan(), other.AsSpan());
+			return ReferenceEquals(source, other) ? 0 : HammingDistance<Char>(source.AsSpan(), other.AsSpan());
 		}
 
 		/// <summary>
@@ -26,7 +27,7 @@ namespace Stringier {
 			Guard.NotNull(source, nameof(source));
 			Guard.NotNull(other, nameof(other));
 			Guard.Equal(source.Length, nameof(source), other.Length, nameof(other));
-			return Unsafe.HammingDistance(source.AsSpan(), other);
+			return HammingDistance<Char>(source.AsSpan(), other);
 		}
 
 		/// <summary>
@@ -39,7 +40,7 @@ namespace Stringier {
 			Guard.NotNull(source, nameof(source));
 			Guard.NotNull(other, nameof(other));
 			Guard.Equal(source.Length, nameof(source), other.Length, nameof(other));
-			return Unsafe.HammingDistance(source, other.AsSpan());
+			return HammingDistance<Char>(source, other.AsSpan());
 		}
 
 		/// <summary>
@@ -51,7 +52,7 @@ namespace Stringier {
 		public static Int32 HammingDistance(String source, ReadOnlySpan<Char> other) {
 			Guard.NotNull(source, nameof(source));
 			Guard.Equal(source.Length, nameof(source), other.Length, nameof(other));
-			return Unsafe.HammingDistance(source.AsSpan(), other);
+			return HammingDistance<Char>(source.AsSpan(), other);
 		}
 
 		/// <summary>
@@ -63,7 +64,7 @@ namespace Stringier {
 		public static Int32 HammingDistance(ReadOnlySpan<Char> source, String other) {
 			Guard.NotNull(other, nameof(other));
 			Guard.Equal(source.Length, nameof(source), other.Length, nameof(other));
-			return Unsafe.HammingDistance(source, other.AsSpan());
+			return HammingDistance<Char>(source, other.AsSpan());
 		}
 
 		/// <summary>
@@ -76,7 +77,7 @@ namespace Stringier {
 			Guard.NotNull(source, nameof(source));
 			Guard.NotNull(other, nameof(other));
 			Guard.Equal(source.Length, nameof(source), other.Length, nameof(other));
-			return ReferenceEquals(source, other) ? 0 : Unsafe.HammingDistance<Char>(source, other);
+			return ReferenceEquals(source, other) ? 0 : HammingDistance<Char>(source, other);
 		}
 
 		/// <summary>
@@ -88,7 +89,7 @@ namespace Stringier {
 		public static Int32 HammingDistance(Char[] source, ReadOnlySpan<Char> other) {
 			Guard.NotNull(source, nameof(source));
 			Guard.Equal(source.Length, nameof(source), other.Length, nameof(other));
-			return Unsafe.HammingDistance(source, other);
+			return HammingDistance<Char>(source, other);
 		}
 
 		/// <summary>
@@ -100,7 +101,7 @@ namespace Stringier {
 		public static Int32 HammingDistance(ReadOnlySpan<Char> source, Char[] other) {
 			Guard.NotNull(other, nameof(other));
 			Guard.Equal(source.Length, nameof(source), other.Length, nameof(other));
-			return Unsafe.HammingDistance(source, other);
+			return HammingDistance<Char>(source, other);
 		}
 
 		/// <summary>
@@ -111,7 +112,28 @@ namespace Stringier {
 		/// <returns>The number of edits to get from <paramref name="source"/> to <paramref name="other"/>.</returns>
 		public static Int32 HammingDistance(ReadOnlySpan<Char> source, ReadOnlySpan<Char> other) {
 			Guard.Equal(source.Length, nameof(source), other.Length, nameof(other));
-			return Unsafe.HammingDistance(source, other);
+			return HammingDistance<Char>(source, other);
+		}
+
+		/// <summary>
+		/// Calculates the Hamming edit-distance, <typeparamref name="T"/>-wise, between the <paramref name="source"/> and <paramref name="other"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of elements being analyzed.</typeparam>
+		/// <param name="source">The source <see cref="ReadOnlySpan{T}"/> of <typeparamref name="T"/>.</param>
+		/// <param name="other">The other <see cref="ReadOnlySpan{T}"/> of <typeparamref name="T"/>.</param>
+		/// <returns>The number of edits to get from <paramref name="source"/> to <paramref name="other"/>.</returns>
+		/// <remarks>
+		/// This isn't safe, and is always inlined. It's meant to be used to build up type specific algorithms.
+		/// </remarks>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Int32 HammingDistance<T>(ReadOnlySpan<T> source, ReadOnlySpan<T> other) where T : IEquatable<T> {
+			Int32 d = 0;
+			for (Int32 i = 0; i < source.Length; i++) {
+				if (!source[i].Equals(other[i])) {
+					d++;
+				}
+			}
+			return d;
 		}
 	}
 }
--- Core/Unsafe.cs
+++ Core/Unsafe.cs
@@ -9,43 +9,22 @@ namespace Stringier {
 	/// <para>These are typically unsafe because they aren't type safe, don't have guarded parameters, etc. Whether generic or weakly typed, they are highly adaptable though. Intended use is to use this to implement a safe variant of the method, or as part of larger safe methods.</para>
 	/// <para>These also slightly optimize many cases, as type information isn't necessary so the creation of a struct or ref struct never needs to happen.</para>
 	/// </remarks>
-	public static class Unsafe {
-		/// <summary>
-		/// Calculates the Hamming edit-distance, <typeparamref name="T"/>-wise, between the <paramref name="source"/> and <paramref name="other"/>.
-		/// </summary>
-		/// <typeparam name="T">The type of elements being analyzed.</typeparam>
-		/// <param name="source">The source <see cref="ReadOnlySpan{T}"/> of <typeparamref name="T"/>.</param>
-		/// <param name="other">The other <see cref="ReadOnlySpan{T}"/> of <typeparamref name="T"/>.</param>
-		/// <returns>The number of edits to get from <paramref name="source"/> to <paramref name="other"/>.</returns>
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Int32 HammingDistance<T>(ReadOnlySpan<T> source, ReadOnlySpan<T> other) where T : IEquatable<T> {
-			Int32 d = 0;
-			for (Int32 i = 0; i < source.Length; i++) {
-				if (!source[i].Equals(other[i])) {
-					d++;
-				}
-			}
-			return d;
-		}
-
+	internal static class Unsafe {
 		/// <summary>
 		/// Is the <paramref name="codePoint"/> ASCII?
 		/// </summary>
 		/// <param name="codePoint">The <see cref="UInt32"/> interpreted as a codepoint.</param>
 		/// <returns><see langword="true"/> if ASCII; otherwise, <see langword="false"/>.</returns>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Boolean IsAscii(UInt32 codePoint) => codePoint <= 0x7Fu;
+		internal static Boolean IsAscii(UInt32 codePoint) => codePoint <= 0x7Fu;
 
 		/// <summary>
 		/// Is the <paramref name="codePoint"/> in the Basic Multilingual Plane?
 		/// </summary>
-		/// <param name="codePoint"></param>
 		/// <param name="codePoint">The <see cref="UInt32"/> interpreted as a codepoint.</param>
 		/// <returns><see langword="true"/> if in the Basic Multilingual Plane; otherwise, <see langword="false"/>.</returns>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Boolean IsBmp(UInt32 codePoint) => Plane(codePoint) == 0;
+		internal static Boolean IsBmp(UInt32 codePoint) => Plane(codePoint) == 0;
 
 		/// <summary>
 		/// Is the <paramref name="value"/> a codepoint?
@@ -53,106 +32,63 @@ namespace Stringier {
 		/// <param name="value">The <see cref="Int32"/> value.</param>
 		/// <returns><see langword="true"/> if codepoint; otherwise, <see langword="false"/>.</returns>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Boolean IsCodePoint(Int32 value) => value >= 0 && IsCodePoint((UInt32)value);
+		internal static Boolean IsCodePoint(Int32 value) => value >= 0 && IsCodePoint((UInt32)value);
 
 		/// <summary>
 		/// Is the <paramref name="value"/> a codepoint?
 		/// </summary>
 		/// <param name="value">The <see cref="UInt32"/> value.</param>
 		/// <returns><see langword="true"/> if codepoint; otherwise, <see langword="false"/>.</returns>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Boolean IsCodePoint(UInt32 value) => value <= 0x10FFFFu;
+		internal static Boolean IsCodePoint(UInt32 value) => value <= 0x10FFFFu;
 
 		/// <summary>
 		/// Is the <paramref name="codePoint"/> a high surrogate?
 		/// </summary>
 		/// <param name="codePoint">The <see cref="UInt32"/> interpreted as a codepoint.</param>
 		/// <returns><see langword="true"/> if high surrogate; otherwise, <see langword="false"/>.</returns>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Boolean IsHighSurrogate(UInt32 codePoint) => unchecked(codePoint - 0xD800u <= 0x03FFu);
+		internal static Boolean IsHighSurrogate(UInt32 codePoint) => unchecked(codePoint - 0xD800u <= 0x03FFu);
 
 		/// <summary>
 		/// Is the <paramref name="codePoint"/> a low surrogate?
 		/// </summary>
 		/// <param name="codePoint">The <see cref="UInt32"/> interpreted as a codepoint.</param>
 		/// <returns><see langword="true"/> if low surrogate; otherwise, <see langword="false"/>.</returns>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Boolean IsLowSurrogate(UInt32 codePoint) => unchecked(codePoint - 0xDC00u <= 0x03FFu);
+		internal static Boolean IsLowSurrogate(UInt32 codePoint) => unchecked(codePoint - 0xDC00u <= 0x03FFu);
 
 		/// <summary>
 		/// Is the <paramref name="codePoint"/> a UNICODE Scalar Value?
 		/// </summary>
 		/// <param name="codePoint">The <see cref="UInt32"/> interpreted as a codepoint.</param>
 		/// <returns><see langword="true"/> if a UNICODE Scalar Value; otherwise, <see langword="false"/>.</returns>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Boolean IsScalarValue(UInt32 codePoint) => ((codePoint - 0x110000u) ^ 0xD800u) >= 0xFFEF0800u;
+		internal static Boolean IsScalarValue(UInt32 codePoint) => ((codePoint - 0x110000u) ^ 0xD800u) >= 0xFFEF0800u;
 
 		/// <summary>
 		/// Is the <paramref name="codePoint"/> in the Supplimentary Multilingual Plane?
 		/// </summary>
 		/// <param name="codePoint">The <see cref="UInt32"/> interpreted as a codepoint.</param>
 		/// <returns><see langword="true"/> if in the Supplimentary Multilingual Plane; otherwise, <see langword="false"/>.</returns>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Boolean IsSmp(UInt32 codePoint) => Plane(codePoint) == 1;
+		internal static Boolean IsSmp(UInt32 codePoint) => Plane(codePoint) == 1;
 
 		/// <summary>
 		/// Is the <paramref name="codePoint"/> a surrogate value?
 		/// </summary>
 		/// <param name="codePoint">The <see cref="UInt32"/> interpreted as a codepoint.</param>
 		/// <returns><see langword="true"/> if a surrogate value; otherwise, <see langword="false"/>.</returns>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Boolean IsSurrogate(UInt32 codePoint) => codePoint.Within(0xD800u, 0xDFFFu);
-
-		/// <summary>
-		/// Calculates the Levenshtein edit-distance, <typeparamref name="T"/>-wise, between the <paramref name="source"/> and <paramref name="other"/>.
-		/// </summary>
-		/// <typeparam name="T">The type of elements being analyzed.</typeparam>
-		/// <param name="source">The source <see cref="ReadOnlySpan{T}"/> of <typeparamref name="T"/>.</param>
-		/// <param name="other">The other <see cref="ReadOnlySpan{T}"/> of <typeparamref name="T"/>.</param>
-		/// <returns>The number of edits to get from <paramref name="source"/> to <paramref name="other"/>.</returns>
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Int32 LevenshteinDistance<T>(ReadOnlySpan<T> source, ReadOnlySpan<T> other) where T : IEquatable<T> {
-			Int32 n = source.Length;
-			Int32 m = other.Length;
-
-			if (n == 0) {
-				return m;
-			} else if (m == 0) {
-				return n;
-			} else {
-				Int32[,] d = new Int32[n + 1, m + 1];
-				for (Int32 i = 0; i <= n; d[i, 0] = i++) { /* Prework */ }
-				for (Int32 j = 0; j <= m; d[0, j] = j++) { /* Prework */ }
-
-				Int32 c;
-				for (Int32 i = 1; i <= n; i++) {
-					for (Int32 j = 1; j <= m; j++) {
-						c = other[j - 1].Equals(source[i - 1]) ? 0 : 1;
-						d[i, j] = Math.Min(
-							Math.Min(
-								d[i - 1, j] + 1,
-								d[i, j - 1] + 1),
-							d[i - 1, j - 1] + c);
-					}
-				}
-				return d[n, m];
-			}
-		}
+		internal static Boolean IsSurrogate(UInt32 codePoint) => codePoint.Within(0xD800u, 0xDFFFu);
 
 		/// <summary>
 		/// Which UNICODE Plane does the <paramref name="codePoint"/> belong to?
 		/// </summary>
 		/// <param name="codePoint">The <see cref="UInt32"/> interpreted as a codepoint.</param>
 		/// <returns>A <see cref="UInt32"/> value corresponding to the UNICODE Plane.</returns>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static UInt32 Plane(UInt32 codePoint) => unchecked(codePoint >> 16);
+		internal static UInt32 Plane(UInt32 codePoint) => unchecked(codePoint >> 16);
 
 		/// <summary>
 		/// Decodes the UTF-16 Surrogate Pairs into a UNICODE Scalar Value.
@@ -163,9 +99,8 @@ namespace Stringier {
 		/// <remarks>
 		/// This method does not do any validity checking what-so-ever. You get a value back. That value may be incorrect, and there's no error to tell you if it is.
 		/// </remarks>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static UInt32 Utf16Decode(UInt32 highSurrogate, UInt32 lowSurrogate) => unchecked((highSurrogate << 10) + lowSurrogate - ((0xD800u << 10) + 0xDC00u - (1 << 16)));
+		internal static UInt32 Utf16Decode(UInt32 highSurrogate, UInt32 lowSurrogate) => unchecked((highSurrogate << 10) + lowSurrogate - ((0xD800u << 10) + 0xDC00u - (1 << 16)));
 
 		/// <summary>
 		/// Encodes the UNICODE Scalar Value into UTF-16 high and low surrogates, assuming <paramref name="smpScalarValue"/> is in the Supplimentary Multilingual Plane.
@@ -176,9 +111,8 @@ namespace Stringier {
 		/// <remarks>
 		/// This method does not do any validity checking what-so-ever. You get a surrogate pair back. Those may be incorrect. They may not even be actual surrogates. And there's no error to tell you if a problem occured.
 		/// </remarks>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static void Utf16Encode(UInt32 smpScalarValue, out Char highSurrogate, out Char lowSurrogate) {
+		internal static void Utf16Encode(UInt32 smpScalarValue, out Char highSurrogate, out Char lowSurrogate) {
 			highSurrogate = (Char)unchecked((smpScalarValue + ((0xD800u - 0x40u) << 10)) >> 10);
 			lowSurrogate = (Char)unchecked((smpScalarValue & 0x03FFu) + 0xDC00u);
 		}
@@ -191,9 +125,8 @@ namespace Stringier {
 		/// <remarks>
 		/// This method does not do any validity checking what-so-ever. You get a sequence length back. That length may be incorrect, especially if <paramref name="scalarValue"/> is not actually a UNICODE Scalar Value. And there's no error to tell you if a problem occured.
 		/// </remarks>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static UInt32 Utf16SequenceLength(UInt32 scalarValue) {
+		internal static UInt32 Utf16SequenceLength(UInt32 scalarValue) {
 			unchecked {
 				scalarValue -= 0x10000u;
 				scalarValue += 2 << 24;
@@ -210,9 +143,8 @@ namespace Stringier {
 		/// <remarks>
 		/// This method does not do any validity checking what-so-ever. You get a sequence length back. That length may be incorrect, especially if <paramref name="scalarValue"/> is not actually a UNICODE Scalar Value. And there's no error to tell you if a problem occured.
 		/// </remarks>
-		[CLSCompliant(false)]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static UInt32 Utf8SequenceLength(UInt32 scalarValue) {
+		internal static UInt32 Utf8SequenceLength(UInt32 scalarValue) {
 			unchecked {
 				Int32 a = ((Int32)scalarValue - 0x0800) >> 31;
 				scalarValue ^= 0xF800u;
