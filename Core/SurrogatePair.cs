efcaf59a9c7f7b01ce365365e6e7d100498ff863
Patrick Kelly <p-kell@live.com>
2020-03-10 18:26:17 -0400

    Some fixes, per analyzers

--- Core/SurrogatePair.cs
+++ Core/SurrogatePair.cs
@@ -6,7 +6,7 @@ namespace Stringier {
 	/// Represents a UTF-16 Surrogate Pair.
 	/// </summary>
 	[StructLayout(LayoutKind.Auto)]
-	public readonly struct SurrogatePair {
+	public readonly struct SurrogatePair : IEquatable<SurrogatePair> {
 		/// <summary>
 		/// The high surrogate
 		/// </summary>
@@ -54,5 +54,41 @@ namespace Stringier {
 		/// Gets the <see cref="CodePoint"/> this <see cref="SurrogatePair"/> represents.
 		/// </summary>
 		public CodePoint CodePoint => new CodePoint(Unsafe.Utf16Decode(High.value, Low.value));
+
+		/// <summary>
+		/// Returns a value that indicates whether this instance is equal to a specified object.
+		/// </summary>
+		/// <param name="obj">The object to compare to.</param>
+		/// <returns><see langword="true"/> if equal to the value of this instance; otherwise, <see langword="false"/>.</returns>
+		public override Boolean Equals(Object obj) {
+			switch (obj) {
+			case SurrogatePair other:
+				return Equals(other);
+			default:
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Returns a value that indicates whether this instance is equal to a specified object.
+		/// </summary>
+		/// <param name="other">The object to compare to.</param>
+		/// <returns><see langword="true"/> if equal to the value of this instance; otherwise, <see langword="false"/>.</returns>
+		public Boolean Equals(SurrogatePair other) => High.Equals(other.High) && Low.Equals(other.Low);
+
+		/// <summary>
+		/// Returns the hash code for this instance.
+		/// </summary>
+		/// <returns>A 32-bit signed integer hash code.</returns>
+		/// <remarks>
+		/// The hash code of a <see cref="CodePoint"/> is always the integer value of the code point.
+		/// </remarks>
+		public override Int32 GetHashCode() => High.GetHashCode() ^ Low.GetHashCode();
+
+		/// <summary>
+		/// Converts the value of this instance to its equivalent string representation.
+		/// </summary>
+		/// <returns>The string representation of the value of this instance.</returns>
+		public override String ToString() => $"({High}, {Low})";
 	}
 }
