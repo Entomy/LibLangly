e25515b0c6a7aff5567fdf15e4297a3e050295b6
Patrick Kelly <p-kell@live.com>
2020-10-28 13:17:17 -0400

    Migrated Doubly-Linked List

---
+++ Collectathon/Lists/DoublyLinkedList`1.Node.cs
@@ -0,0 +1,22 @@
+﻿namespace Collectathon.Lists {
+	public partial class DoublyLinkedList<TElement> {
+		/// <summary>
+		/// Represents a node of a <see cref="DoublyLinkedList{TElement}"/>.
+		/// </summary>
+		public sealed class Node : LinkedNode<TElement, Node> {
+			/// <summary>
+			/// The previous <see cref="Node"/>.
+			/// </summary>
+			public Node Previous;
+
+			/// <inheritdoc/>
+			public Node(TElement element, Node next, Node previous) : base(element, next) => Previous = previous;
+
+			/// <inheritdoc/>
+			public override void Clear() {
+				Next = null;
+				Previous = null;
+			}
+		}
+	}
+}
