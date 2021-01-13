f53768e50c5bc3966a67cd2c9aa36c761cdbc547
Patrick Kelly <p-kell@live.com>
2021-01-13 09:38:05 -0500

    Reworked Unsafe static class to be a shared project

---
+++ Libraries/Langly.Unsafe/UTF16Decode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Langly {
+	internal static partial class Unsafe {
+		/// <summary>
+		/// Decodes the UTF-16 Surrogate Pairs into a UNICODE Scalar Value.
+		/// </summary>
+		/// <param name="highSurrogate">The <see cref="UInt32"/> interpreted as the high surrogate.</param>
+		/// <param name="lowSurrogate">The <see cref="UInt32"/> interpreted as the low surrogate.</param>
+		/// <returns>A <see cref="UInt32"/> representing the UNICODE Scalar Value.</returns>
+		/// <remarks>
+		/// This method does not do any validity checking what-so-ever. You get a value back. That value may be incorrect, and there's no error to tell you if it is.
+		/// </remarks>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static UInt32 UTF16Decode(UInt32 highSurrogate, UInt32 lowSurrogate) => unchecked((highSurrogate << 10) + lowSurrogate - ((0xD800u << 10) + 0xDC00u - (1 << 16)));
+	}
+}
---
+++ Libraries/Langly.Unsafe/Unsafe.cs
@@ -0,0 +1,10 @@
+﻿namespace Langly {
+	/// <summary>
+	/// Unsafe variants of algorithms.
+	/// </summary>
+	/// <remarks>
+	/// <para>These are typically unsafe because they aren't type safe, don't have guarded parameters, etc. Whether generic or weakly typed, they are highly adaptable though. Intended use is to use this to implement a safe variant of the method for downstream use, but the unsafe method internally.</para>
+	/// <para>These also slightly optimize many cases, as type information isn't necessary so the creation of a struct or ref struct never needs to happen.</para>
+	/// </remarks>
+	internal static partial class Unsafe { }
+}
