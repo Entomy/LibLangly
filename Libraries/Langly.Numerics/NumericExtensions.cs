5e5b68dc8a2e289918d3931394c480e948c5f23f
Patrick Kelly <p-kell@live.com>
2021-01-12 20:47:49 -0500

    Added basic numerics support in the form of Sum and Max for number sequences

---
+++ Libraries/Langly.Numerics/NumericExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Langly {
+	/// <summary>
+	/// Provides extensions for core traits when used with numeric types.
+	/// </summary>
+	public static partial class NumericExtensions { }
+}
--- Tests/DataStructures/Lists/Chain`1_Tests.cs
+++ Tests/DataStructures/Lists/Chain`1_Tests.cs
@@ -171,6 +171,157 @@ namespace Langly.DataStructures.Lists {
 		}
 
 		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Int32[] { })]
+		[InlineData(5, new Int32[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new Int32[] { 5, 4, 3, 2, 1 })]
+		public void Max_nint(Int32 expected, Int32[] values) {
+			nint[] vals = null;
+			if (values is not null) {
+				vals = new nint[values.Length];
+				for (nint i = 0; i < values.Length; i++) {
+					vals[i] = (nint)values[i];
+				}
+			}
+			Chain<nint> chain = new Chain<nint>().Add(vals);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new UInt32[] { })]
+		[InlineData(5, new UInt32[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new UInt32[] { 5, 4, 3, 2, 1 })]
+		public void Max_nuint(UInt32 expected, UInt32[] values) {
+			nuint[] vals = null;
+			if (values is not null) {
+				vals = new nuint[values.Length];
+				for (nint i = 0; i < values.Length; i++) {
+					vals[i] = (nuint)values[i];
+				}
+			}
+			Chain<nuint> chain = new Chain<nuint>().Add(vals);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new SByte[] { })]
+		[InlineData(5, new SByte[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new SByte[] { 5, 4, 3, 2, 1 })]
+		public void Max_SByte(Int32 expected, SByte[] values) {
+			Chain<SByte> chain = new Chain<SByte>().Add(values);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Byte[] { })]
+		[InlineData(5, new Byte[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new Byte[] { 5, 4, 3, 2, 1 })]
+		public void Max_Byte(UInt32 expected, Byte[] values) {
+			Chain<Byte> chain = new Chain<Byte>().Add(values);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Int16[] { })]
+		[InlineData(5, new Int16[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new Int16[] { 5, 4, 3, 2, 1 })]
+		public void Max_Int16(Int32 expected, Int16[] values) {
+			Chain<Int16> chain = new Chain<Int16>().Add(values);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new UInt16[] { })]
+		[InlineData(5, new UInt16[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new UInt16[] { 5, 4, 3, 2, 1 })]
+		public void Max_UInt16(UInt32 expected, UInt16[] values) {
+			Chain<UInt16> chain = new Chain<UInt16>().Add(values);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Int32[] { })]
+		[InlineData(5, new Int32[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new Int32[] { 5, 4, 3, 2, 1 })]
+		public void Max_Int32(Int32 expected, Int32[] values) {
+			Chain<Int32> chain = new Chain<Int32>().Add(values);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new UInt32[] { })]
+		[InlineData(5, new UInt32[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new UInt32[] { 5, 4, 3, 2, 1 })]
+		public void Max_UInt32(UInt32 expected, UInt32[] values) {
+			Chain<UInt32> chain = new Chain<UInt32>().Add(values);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Int64[] { })]
+		[InlineData(5, new Int64[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new Int64[] { 5, 4, 3, 2, 1 })]
+		public void Max_Int64(Int64 expected, Int64[] values) {
+			Chain<Int64> chain = new Chain<Int64>().Add(values);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new UInt64[] { })]
+		[InlineData(5, new UInt64[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new UInt64[] { 5, 4, 3, 2, 1 })]
+		public void Max_UInt64(UInt64 expected, UInt64[] values) {
+			Chain<UInt64> chain = new Chain<UInt64>().Add(values);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Single[] { })]
+		[InlineData(5, new Single[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new Single[] { 5, 4, 3, 2, 1 })]
+		public void Max_Single(Single expected, Single[] values) {
+			Chain<Single> chain = new Chain<Single>().Add(values);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Double[] { })]
+		[InlineData(5, new Double[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new Double[] { 5, 4, 3, 2, 1 })]
+		public void Max_Double(Double expected, Double[] values) {
+			Chain<Double> chain = new Chain<Double>().Add(values);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Int32[] { })]
+		[InlineData(5, new Int32[] { 1, 2, 3, 4, 5 })]
+		[InlineData(5, new Int32[] { 5, 4, 3, 2, 1 })]
+		public void Max_Decimal(Decimal expected, Int32[] values) {
+			Decimal[] vals = null;
+			if (values is not null) {
+				vals = new Decimal[values.Length];
+				for (nint i = 0; i < values.Length; i++) {
+					vals[i] = values[i];
+				}
+			}
+			Chain<Decimal> chain = new Chain<Decimal>().Add(vals);
+			Assert.Equal(expected, chain.Max());
+		}
+
+		[Theory]
 		[InlineData(new Int32[] { }, new Int32[] { }, 0, 0)]
 		[InlineData(new Int32[] { 1, 2, 3, 4, 5 }, new Int32[] { 1, 2, 3, 4, 5 }, 0, 0)]
 		[InlineData(new Int32[] { 0, 2, 3, 4, 5 }, new Int32[] { 1, 2, 3, 4, 5 }, 1, 0)]
@@ -231,6 +382,144 @@ namespace Langly.DataStructures.Lists {
 			}
 			_ = chain.Replace(search, replace);
 			Assert.Equal(expected, chain);
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Int32[] { })]
+		[InlineData(15, new Int32[] { 1, 2, 3, 4, 5 })]
+		public void Sum_nint(Int32 expected, Int32[] values) {
+			nint[] vals = null;
+			if (values is not null) {
+				vals = new nint[values.Length];
+				for (nint i = 0; i < values.Length; i++) {
+					vals[i] = (nint)values[i];
+				}
+			}
+			Chain<nint> chain = new Chain<nint>().Add(vals);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new UInt32[] { })]
+		[InlineData(15, new UInt32[] { 1, 2, 3, 4, 5 })]
+		public void Sum_nuint(UInt32 expected, UInt32[] values) {
+			nuint[] vals = null;
+			if (values is not null) {
+				vals = new nuint[values.Length];
+				for (nint i = 0; i < values.Length; i++) {
+					vals[i] = (nuint)values[i];
+				}
+			}
+			Chain<nuint> chain = new Chain<nuint>().Add(vals);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new SByte[] { })]
+		[InlineData(15, new SByte[] { 1, 2, 3, 4, 5 })]
+		public void Sum_SByte(Int32 expected, SByte[] values) {
+			Chain<SByte> chain = new Chain<SByte>().Add(values);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Byte[] { })]
+		[InlineData(15, new Byte[] { 1, 2, 3, 4, 5 })]
+		public void Sum_Byte(UInt32 expected, Byte[] values) {
+			Chain<Byte> chain = new Chain<Byte>().Add(values);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Int16[] { })]
+		[InlineData(15, new Int16[] { 1, 2, 3, 4, 5 })]
+		public void Sum_Int16(Int32 expected, Int16[] values) {
+			Chain<Int16> chain = new Chain<Int16>().Add(values);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new UInt16[] { })]
+		[InlineData(15, new UInt16[] { 1, 2, 3, 4, 5 })]
+		public void Sum_UInt16(UInt32 expected, UInt16[] values) {
+			Chain<UInt16> chain = new Chain<UInt16>().Add(values);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Int32[] { })]
+		[InlineData(15, new Int32[] { 1, 2, 3, 4, 5 })]
+		public void Sum_Int32(Int32 expected, Int32[] values) {
+			Chain<Int32> chain = new Chain<Int32>().Add(values);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new UInt32[] { })]
+		[InlineData(15, new UInt32[] { 1, 2, 3, 4, 5 })]
+		public void Sum_UInt32(UInt32 expected, UInt32[] values) {
+			Chain<UInt32> chain = new Chain<UInt32>().Add(values);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Int64[] { })]
+		[InlineData(15, new Int64[] { 1, 2, 3, 4, 5 })]
+		public void Sum_Int64(Int64 expected, Int64[] values) {
+			Chain<Int64> chain = new Chain<Int64>().Add(values);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new UInt64[] { })]
+		[InlineData(15, new UInt64[] { 1, 2, 3, 4, 5 })]
+		public void Sum_UInt64(UInt64 expected, UInt64[] values) {
+			Chain<UInt64> chain = new Chain<UInt64>().Add(values);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Single[] { })]
+		[InlineData(15, new Single[] { 1, 2, 3, 4, 5 })]
+		public void Sum_Single(Single expected, Single[] values) {
+			Chain<Single> chain = new Chain<Single>().Add(values);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Double[] { })]
+		[InlineData(15, new Double[] { 1, 2, 3, 4, 5 })]
+		public void Sum_Double(Double expected, Double[] values) {
+			Chain<Double> chain = new Chain<Double>().Add(values);
+			Assert.Equal(expected, chain.Sum());
+		}
+
+		[Theory]
+		[InlineData(0, null)]
+		[InlineData(0, new Int32[] { })]
+		[InlineData(15, new Int32[] { 1, 2, 3, 4, 5 })]
+		public void Sum_Decimal(Decimal expected, Int32[] values) {
+			Decimal[] vals = null;
+			if (values is not null) {
+				vals = new Decimal[values.Length];
+				for (nint i = 0; i < values.Length; i++) {
+					vals[i] = values[i];
+				}
+			}
+			Chain<Decimal> chain = new Chain<Decimal>().Add(vals);
+			Assert.Equal(expected, chain.Sum());
 		}
 	}
 }
