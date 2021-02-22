397cd53a2378f8b554a6a05923ba7630cb509465
Patrick Kelly <p-kell@live.com>
2021-02-22 09:13:10 -0500

    Added in all the migratory meta-packages

---
+++ Packages/Consolator/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(true)]
+[assembly: Guid("9A62F155-D607-4992-93B1-F71ECBDA1162")]
---
+++ Packages/Philosoft/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(true)]
+[assembly: Guid("B1B1959D-DE43-49C8-BA71-55F39B2318E2")]
+
