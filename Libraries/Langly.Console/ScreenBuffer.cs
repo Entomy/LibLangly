d70ecd6034be051f4049f24277b3aca612751f51
Patrick Kelly <p-kell@live.com>
2021-03-15 13:24:18 -0400

    A few more global suppressions

--- Libraries/Langly.Console/ScreenBuffer.cs
+++ Libraries/Langly.Console/ScreenBuffer.cs
@@ -10,6 +10,7 @@ namespace Langly {
 	/// This is not actually a buffer, but rather, a proxy to assist the programmer in managing and understanding the lifetime of alternate screen buffer. Furthermore, because it is exposed as a non-static type, it is possible to extend the buffer features by third-party libraries. Because extension methods would have to be called off this type, and if this type exists the buffer is currently active, then screen drawing techniques which apply only to the alternate screen buffer are greatly simplified.
 	/// </remarks>
 	[SuppressMessage("IDisposableAnalyzers.Correctness", "IDISP009:Add IDisposable interface.", Justification = "We can't apply interfaces on ref structs.")]
+	[SuppressMessage("Design", "CC0091:Use static method", Justification = "I fully get the reasoning, but these should show up off the instance to remain orthogonal with extensions. If you're not using the alternate screen buffer, you'd be using Console with its static calls.")]
 	public ref struct ScreenBuffer {
 		/// <summary>
 		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
