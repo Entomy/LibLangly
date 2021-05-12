b6eb6ee81d2f714b0b634b35e2fa85f3a8b884dc
Patrick Kelly <p-kell@live.com>
2021-05-12 13:41:59 -0400

    Migrated over edit distance algorithms

---
+++ Tests.CSharp/EditDistance_Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Stringier;
+using Xunit;
+
+namespace Langly {
+	public class EditDistanceTests {
+		[Theory]
+		[InlineData("ram", "ram", 0)]
+		[InlineData("ram", "rom", 1)]
+		[InlineData("ram", "rob", 2)]
+		[InlineData("ram", "rma", 2)]
+		public void Hamming(String source, String other, Int32 expected) => Assert.Equal(expected, EditDistance.Hamming(source, other));
+
+		[Theory]
+		[InlineData("ram", "rom", 1)]
+		[InlineData("\u0072\u0061\u006D", "\u0072\u0061\u0301\u006D", 1)]
+		[InlineData("\u0072\u00E1\u006D", "\u0072\u0061\u0301\u006D", 2)]
+		[InlineData("ram", "rob", 2)]
+		[InlineData("ram", "random", 3)]
+		[InlineData("flaw", "lawn", 2)]
+		public void Levenshtein(String source, String other, Int32 expected) => Assert.Equal(expected, EditDistance.Levenshtein(source, other));
+	}
+}
