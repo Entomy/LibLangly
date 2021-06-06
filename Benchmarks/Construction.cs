3c071722a33f04550caac5e4868829ba63a66f3d
Patrick Kelly <p-kell@live.com>
2021-06-06 06:31:59 -0400

    Adding benchmarks back in

---
+++ Benchmarks/Construction.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Collectathon.Arrays;
+using Collectathon.Lists;
+using Collectathon.Queues;
+using Collectathon.Stacks;
+using Numbersome;
+using MSFT = System.Collections.Generic;
+
+namespace Langly {
+	[SimpleJob(RuntimeMoniker.Net50)]
+	[MemoryDiagnoser]
+	public class Construction {
+		[Benchmark(Baseline = true)]
+		public void Array() => _ = new Int32[32];
+
+		[Benchmark]
+		public void C5_ArrayList() => _ = new C5.ArrayList<Int32>(32);
+
+		[Benchmark]
+		public void C5_CircularQueue() => _ = new C5.CircularQueue<Int32>(32);
+
+		[Benchmark]
+		public void C5_HashBag() => _ = new C5.HashBag<Int32>();
+
+		[Benchmark]
+		public void C5_HashDictionary() => _ = new C5.HashDictionary<Char, String>();
+
+		[Benchmark]
+		public void C5_HashedArrayList() => _ = new C5.HashedArrayList<Int32>(32);
+
+		[Benchmark]
+		public void C5_HashedLinkedList() => _ = new C5.HashedLinkedList<Int32>();
+
+		[Benchmark]
+		public void C5_HashSet() => _ = new C5.HashSet<Int32>();
+
+		[Benchmark]
+		public void C5_IntervalHeap() => _ = new C5.IntervalHeap<Int32>(32);
+
+		[Benchmark]
+		public void C5_LinkedList() => _ = new C5.LinkedList<Int32>();
+
+		[Benchmark]
+		public void C5_SortedArray() => _ = new C5.SortedArray<Int32>(32);
+
+		[Benchmark]
+		public void C5_TreeBag() => _ = new C5.TreeBag<Int32>();
+
+		[Benchmark]
+		public void C5_TreeDictionary() => _ = new C5.TreeDictionary<Char, String>();
+
+		[Benchmark]
+		public void C5_TreeSet() => _ = new C5.TreeSet<Int32>();
+		[Benchmark]
+		public void Collectathon_AssociativeBoundedArray() => _ = new BoundedArray<Char, String>(32);
+
+		[Benchmark]
+		public void Collectathon_AssociativeDynamicArray() => _ = new DynamicArray<Char, String>(32);
+
+		[Benchmark]
+		public void Collectathon_BoundedArray() => _ = new BoundedArray<Int32>(32);
+
+		[Benchmark]
+		public void Collectathon_DynamicArray() => _ = new DynamicArray<Int32>(32);
+
+		[Benchmark]
+		public void Collectathon_Queue() => _ = new Queue<Int32>();
+
+		[Benchmark]
+		public void Collectathon_SinglyLinkedList() => _ = new SinglyLinkedList<Int32>();
+
+		[Benchmark]
+		public void Collectathon_Stack() => _ = new Stack<Int32>();
+
+		[Benchmark]
+		public void MSFT_Dictionary() => _ = new MSFT.Dictionary<Char, String>(32);
+
+		[Benchmark]
+		public void MSFT_HashSet() => _ = new MSFT.HashSet<Int32>(32);
+
+		[Benchmark]
+		public void MSFT_LinkedList() => _ = new MSFT.LinkedList<Int32>();
+
+		[Benchmark]
+		public void MSFT_List() => _ = new MSFT.List<Int32>();
+
+		[Benchmark]
+		public void MSFT_Queue() => _ = new MSFT.Queue<Int32>(32);
+
+		[Benchmark]
+		public void MSFT_SegmentedList() => _ = new MSFT.SegmentedList<Int32>(8, 32);
+
+		[Benchmark]
+		public void MSFT_SortedDictionary() => _ = new MSFT.SortedDictionary<Char, String>();
+
+		[Benchmark]
+		public void MSFT_SortedSet() => _ = new MSFT.SortedSet<Int32>();
+
+		[Benchmark]
+		public void MSFT_Stack() => _ = new MSFT.Stack<Int32>(32);
+
+		[Benchmark]
+		public void Numbersome_Set() => _ = new Set<Int32>((_) => true);
+	}
+}
+
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using BenchmarkDotNet.Running;
+using Spectre.Console;
+using Console = Spectre.Console.AnsiConsole;
+
+namespace Langly {
+	public static class Program {
+		[SuppressMessage("Performance", "HAA0101:Array allocation for params parameter", Justification = "Yes, this is a simple benchmarking interface, it's fine.")]
+		[SuppressMessage("Performance", "HLQ010:Consider using a 'for' loop instead.", Justification = "Yes, this is a simple benchmarking interface, it's fine.")]
+		public static void Main() {
+			Console.Render(new FigletText("LibLangly").Color(Color.Chartreuse1));
+			Console.Render(new FigletText("Benchmarks").Color(Color.Magenta1));
+			foreach (String selection in Console.Prompt(new MultiSelectionPrompt<String>()
+				.PageSize(10)
+				.NotRequired()
+				.MoreChoicesText("[grey](Move up and down to reveal more benchmarks)[/]")
+				.InstructionsText("[grey](Press [blue]<space>[/] to toggle a benchmark, [green]<enter>[/] to accept and run)[/]")
+				.AddChoiceGroup(nameof(Collectathon), new[] {
+					nameof(Construction),
+				})
+				.AddChoiceGroup(nameof(Langly), new[] {
+					""
+				})
+				.AddChoiceGroup(nameof(Numbersome), new[] {
+					""
+				})
+				.AddChoiceGroup(nameof(Streamy), new[] {
+					""
+				})
+				.AddChoiceGroup(nameof(Stringier), new[] {
+					""
+				}))) {
+				switch (selection) {
+				case nameof(Construction):
+					BenchmarkRunner.Run<Construction>();
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	}
+}
