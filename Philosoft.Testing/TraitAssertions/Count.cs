ee8e062ebcce7085cca96cce6388bb94fcb21ec4
Patrick Kelly <p-kell@live.com>
2021-06-29 18:56:03 -0400

    A few tweaks to assertions.

--- Philosoft.Testing/TraitAssertions/Count.cs
+++ Philosoft.Testing/TraitAssertions/Count.cs
@@ -3,10 +3,10 @@
 		/// <summary>
 		/// Asserts that this instances <see cref="ICount.Count"/> is what was <paramref name="expected"/>.
 		/// </summary>
-		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="T">The type of the collection.</typeparam>
 		/// <param name="assert">This <see cref="Assert{T}"/>.</param>
 		/// <param name="expected"></param>
-		/// <returns></returns>
+		/// <returns>This <paramref name="assert"/>.</returns>
 		public static Assert<T> Count<T>(this Assert<T> assert, Int32 expected) where T : ICount {
 			if (!Equals(assert.Actual.Count, expected)) {
 				throw new AssertException($"The count of elements in the collection was not what was expected.\nActual: {assert.Actual.Count}\nExpected: {expected}");
