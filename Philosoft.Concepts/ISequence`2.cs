5c1de314c6415007737053bf4340f6ef4ecb6551
Patrick Kelly <p-kell@live.com>
2021-07-03 13:21:16 -0400

    ISequence now only requires the traits that make up an enumerator

--- Philosoft.Concepts/ISequence`2.cs
+++ Philosoft.Concepts/ISequence`2.cs
@@ -9,7 +9,7 @@ namespace System.Traits.Concepts {
 	/// <remarks>
 	/// This interface devirtualizes the enumerator, and simplifies numerous parts of the interface through well defined defaults.
 	/// </remarks>
-	public interface ISequence<out TElement, TEnumerator> : ICount where TEnumerator : IEnumerator<TElement> {
+	public interface ISequence<out TElement, TEnumerator> : ICount where TEnumerator : ICount, ICurrent<TElement>, IMoveNext, IReset {
 		/// <summary>
 		/// Returns an enumerator that iterates through the sequence.
 		/// </summary>
