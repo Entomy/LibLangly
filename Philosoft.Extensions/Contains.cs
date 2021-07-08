9d3f7357afb0b15ab3167e10bdc9c4781f3c05da
Patrick Kelly <p-kell@live.com>
2021-07-08 07:25:01 -0400

    More contains ops

--- Philosoft.Extensions/Contains.cs
+++ Philosoft.Extensions/Contains.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Traits;
 using System.Traits.Concepts;
 
 namespace System {
@@ -15,6 +14,23 @@ namespace System {
 		public static Boolean Contains<TElement, TEnumerator>([DisallowNull] this ISequence<TElement, TEnumerator> collection, [AllowNull] TElement element) where TEnumerator : IEnumerator<TElement> {
 			foreach (TElement item in collection) {
 				if (Equals(element, item)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// Determines whether this collection contains any elements described by the <paramref name="predicate"/>.
+		/// </summary>
+		/// <typeparam name="TElement">The type of the elements in the collection.</typeparam>
+		/// <typeparam name="TEnumerator">The type of the enumerator for the collection.</typeparam>
+		/// <param name="collection">This collection.</param>
+		/// <param name="predicate">The predicate describing the element to attempt to find.</param>
+		/// <returns><see langword="true"/> if an element described by the <paramref name="predicate"/> is contained in this collection; otherwise, <see langword="false"/>.</returns>
+		public static Boolean Contains<TElement, TEnumerator>([DisallowNull] this ISequence<TElement, TEnumerator> collection, [AllowNull] Predicate<TElement> predicate) where TEnumerator : IEnumerator<TElement> {
+			foreach (TElement item in collection) {
+				if (predicate?.Invoke(item) ?? item is null) {
 					return true;
 				}
 			}
