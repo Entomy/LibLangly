129ecb5f99baa2949bf9778659dd03e9b187ca1f
Patrick Kelly <p-kell@live.com>
2021-07-10 10:31:46 -0400

    Changed some dependencies, and added Throws and MaybeThrows attributes. Will be working on an analyzer for all of this now.

--- Philosoft.BehavioralAttributes/AssemblyInfo.cs
+++ Philosoft.BehavioralAttributes/AssemblyInfo.cs
@@ -5,4 +5,3 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(true)]
 [assembly: Guid("11A32AF8-D105-43DE-9989-D9EB1508B0A2")] 
-[assembly: ExcludeFromCodeCoverage]
